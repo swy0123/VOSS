4e0003314a015017476b8994a7676a709c3409b6
이시영 <swy05123@gmail.com>
2023-07-18 16:17:30 +0900

    test: 프론트 프로젝트 생성

---
+++ front-end/src/Root.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+import Header from "./components/Header";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+function Root() {
+  return (
+    <Container>
+    {/* <div> */}
+      <Header />
+      <Outlet />
+    {/* </div>  */}
+    </Container>
+  );
+}
+
+export default Root;
