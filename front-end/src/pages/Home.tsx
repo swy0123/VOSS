3972fefea4296001abe1a6564eb6ea1416315bd3
이시영 <swy05123@gmail.com>
2023-07-19 00:06:00 +0900

    feat: 로그인 화면 구현

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,11 +1,20 @@
+import { styled } from "styled-components";
 import Login from "../components/Login";
+import MainImg from "../assets/MainImg.jpg";
+
+const BackgroundImg = styled.div`
+  background-image: url("${MainImg}");
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+`;
 
 function Home() {
   return (
-    <div>
+    <BackgroundImg>
       <h1>Home</h1>
       <Login />
-    </div>
+    </BackgroundImg>
   );
 }
 
