6fdfb27e74cb5e660ca46648f7d38b169abd38d4
이시영 <swy05123@gmail.com>
2023-07-19 00:06:00 +0900

    feat: 로그인 화면 구현

--- front-end/src/components/Login.tsx
+++ front-end/src/components/Login.tsx
@@ -1,23 +1,82 @@
 import React, { ChangeEvent, FormEvent, useState } from "react";
+import { Link } from "react-router-dom";
 import { styled } from "styled-components";
 
 const Container = styled.div`
   background-color: #ffffff;
-  width: 300px;
-  height: 400px;
-  border: #000000;
+  width: 460px;
+  height: 768px;
+  border: #bdbdbd;
   border-style: solid;
-  border-radius: 20px;
+  border-width: 1px;
+  border-radius: 24px 24px 0px 0px;
+  text-align: center;
+  position: fixed;
+  bottom: 0px;
+  right: 15%;
+`;
+
+const P = styled.p`
+  font-size: 12.8px;
+  font-weight: bold;
+`;
+
+const H2 = styled.h2`
+  font-size: 25px;
+  font-weight: bold;
+`;
+
+const Title = styled.div`
+  width: 380px;
+  height: auto;
+  margin: 40px auto;
+  margin-bottom: 100px;
+  text-align: left;
+`;
+
+const UnderText = styled.div`
+  width: 380px;
+  height: auto;
+  margin: 0 auto;
+  margin-top: 200px;
+`;
+
+const InputDiv = styled.div`
+  position: relative;
+`;
+
+const InputHeader = styled.div`
+  width: fit-content;
+  padding: 5px;
+  height: 22;
+  background-color: #ffffff;
+  position: absolute;
+  top: 5px;
+  left: 15%;
 `;
 
 const Input = styled.input`
-  border: #ffffff;
-  border: #000000;
+  border: #bdbdbd;
   border-style: solid;
-  width: 80px;
-  height: 20px;
+  width: 380px;
+  height: 56px;
+  padding: 0px;
   border-width: 1px;
-  border-radius: 10%;
+  border-radius: 8px;
+  margin-top: 25px;
+`;
+
+const Button = styled.button`
+  background-color: #212121;
+  border: #212121;
+  font-size: 12.8px;
+  color: #ffffff;
+  border-style: solid;
+  width: 380px;
+  height: 56px;
+  border-width: 1px;
+  border-radius: 8px;
+  margin-top: 25px;
 `;
 
 function Login() {
@@ -42,25 +101,33 @@ function Login() {
 
   return (
     <Container>
-      <p>LET'S GET YOU STARTED</p>
-      <h2>Create an Account</h2>
+      <Title>
+        <P>LET'S GET YOU STARTED</P>
+        <H2>Create an Account</H2>
+      </Title>
 
       <form onSubmit={handleSubmit}>
-        <div>
-          <div>Username</div>
+        <InputDiv>
+          <InputHeader>Username</InputHeader>
           <Input type="text" onChange={handleUsernameField} placeholder="Username" />
-        </div>
-        <div>
-          <div>Email</div>
-          <Input type="text" onChange={handleEmailField} placeholder="Email" />
-        </div>
-        <div>
-          <div>Password</div>
-          <Input type="text" onChange={handlePasswordField} placeholder="Password" />
-        </div>
+        </InputDiv>
+        <InputDiv>
+          <InputHeader>Email</InputHeader>
+          <Input type="email" onChange={handleEmailField} placeholder="Email" />
+        </InputDiv>
+        <InputDiv>
+          <InputHeader>Password</InputHeader>
+          <Input type="password" onChange={handlePasswordField} placeholder="Password" />
+        </InputDiv>
 
-        <button type="submit">submit</button>
+        <Button type="submit">GET STARTED</Button>
       </form>
+
+      <UnderText>
+        <P>
+          Already have an account? <Link to={"/about"}>LOGIN HERE</Link>
+        </P>
+      </UnderText>
     </Container>
   );
 }
