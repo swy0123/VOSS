98adde07115ee919503a0115ab56b151c07ee3a7
이원영 <sheltonwon@gmail.com>
2023-07-19 11:05:53 +0900

    env: add spring boot project

---
+++ backend/src/test/java/com/yukgaejang/voss/VossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukgaejang.voss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
