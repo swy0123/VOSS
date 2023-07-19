4cafc0e5073fcd5e89a564ada4d6f7365570309b
정현우 <gusehd502@naver.com>
2023-07-19 17:17:03 +0900

    feat: 각 페이지 개설 S09P12B106-118

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter, Routes, Route} from "react-router-dom";
 import Header from "./components/Header";
-import Home from "./pages/Home";
-import SelectCategory from "./pages/SelectCategory";
 import Root from "./Root";
 import ErrorComponent from "./components/ErrorComponent";
 import NotFound from "./pages/NotFound"
+import Home from "./pages/Home";
+import SelectCategory from "./pages/SelectCategory";
+import TrainingVoiceAnalysis from "./pages/TrainingVoiceAnalysis";
+import TrainingDubbing from "./pages/TrainingDubbing";
+import TrainingAccent from "./pages/TrainingAccent";
 
 const router = createBrowserRouter([
     {
@@ -19,6 +22,21 @@ const router = createBrowserRouter([
             {
                 path:"selectcategory",
                 element: <SelectCategory/>,
+                errorElement: <ErrorComponent/>,
+            },
+            {
+                path:"trainingvoiceanalysis",
+                element: <TrainingVoiceAnalysis/>,
+                errorElement: <ErrorComponent/>,
+            },
+            {
+                path:"trainingdubbing",
+                element: <TrainingDubbing/>,
+                errorElement: <ErrorComponent/>,
+            },
+            {
+                path:"trainingaccent",
+                element: <TrainingAccent/>,
                 errorElement: <ErrorComponent/>,
             },
         ],
