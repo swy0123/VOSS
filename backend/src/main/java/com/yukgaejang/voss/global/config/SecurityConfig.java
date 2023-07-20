bf0f400daa0e4387d1076e2ba469c9684b549b51
이원영 <sheltonwon@gmail.com>
2023-07-20 10:04:38 +0900

    feat: jwt 인증 필터 추가 S09P12B106-43

--- backend/src/main/java/com/yukgaejang/voss/global/config/SecurityConfig.java
+++ backend/src/main/java/com/yukgaejang/voss/global/config/SecurityConfig.java
@@ -1,19 +1,36 @@
 package com.yukgaejang.voss.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yukgaejang.voss.domain.auth.service.AuthService;
+import com.yukgaejang.voss.domain.auth.service.LoginFailureHandler;
+import com.yukgaejang.voss.domain.auth.service.LoginSuccessHandler;
+import com.yukgaejang.voss.domain.member.repository.MemberRepository;
+import com.yukgaejang.voss.domain.member.repository.RefreshTokenRepository;
+import com.yukgaejang.voss.global.jwt.service.JwtService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
 public class SecurityConfig {
+    private final AuthService authService;
+    private final JwtService jwtService;
+    private final MemberRepository memberRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final ObjectMapper objectMapper;
+    private final AuthenticationEntryPoint entryPoint;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -32,5 +49,23 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(authService);
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public LoginSuccessHandler loginSuccessHandler() {
+        return new LoginSuccessHandler(jwtService, memberRepository, refreshTokenRepository);
+    }
+
+    @Bean
+    public LoginFailureHandler loginFailureHandler() {
+        return new LoginFailureHandler();
     }
 }
