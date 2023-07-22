d6449c380e5a94b44430b54ce3691fcd45cc2809
210-reverof <sheltonwon@gmail.com>
2023-07-22 23:03:55 +0900

    feat: service 구현체 추가 S09P12B106-59

--- backend/src/main/java/com/yukgaejang/voss/domain/badge/controller/BadgeController.java
+++ backend/src/main/java/com/yukgaejang/voss/domain/badge/controller/BadgeController.java
@@ -3,6 +3,8 @@ package com.yukgaejang.voss.domain.badge.controller;
 import com.yukgaejang.voss.domain.badge.service.BadgeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,8 +17,9 @@ public class BadgeController {
     private final BadgeService badgeService;
 
     @PostMapping
-    public ResponseEntity<Void> badge(@RequestParam("receiver") String receiver) {
-        badgeService.giveBadge(receiver);
+    public ResponseEntity<Void> badge() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        badgeService.giveBadge(authentication.getName());
 
         return ResponseEntity.ok().build();
     }
