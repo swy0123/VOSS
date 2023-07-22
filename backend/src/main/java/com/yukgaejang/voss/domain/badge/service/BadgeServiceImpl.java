7f7a8b64709a5e2d9915643bf3f1446b50cbbc5e
210-reverof <sheltonwon@gmail.com>
2023-07-22 23:03:55 +0900

    feat: service 구현체 추가 S09P12B106-59

---
+++ backend/src/main/java/com/yukgaejang/voss/domain/badge/service/BadgeServiceImpl.java
@@ -0,0 +1,11 @@
+package com.yukgaejang.voss.domain.badge.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BadgeServiceImpl implements BadgeService {
+    @Override
+    public void giveBadge(String receiver) {
+
+    }
+}
