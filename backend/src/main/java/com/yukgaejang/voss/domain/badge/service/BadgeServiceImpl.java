fcc98294edb12c888d7e6a5b08025294459244d7
210-reverof <sheltonwon@gmail.com>
2023-07-23 22:43:57 +0900

    feat: 24시간 제한 추가 S09P12B106-59

--- backend/src/main/java/com/yukgaejang/voss/domain/badge/service/BadgeServiceImpl.java
+++ backend/src/main/java/com/yukgaejang/voss/domain/badge/service/BadgeServiceImpl.java
@@ -10,6 +10,8 @@ import com.yukgaejang.voss.domain.member.repository.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class BadgeServiceImpl implements BadgeService {
@@ -17,10 +19,17 @@ public class BadgeServiceImpl implements BadgeService {
     private final MemberRepository memberRepository;
     private final BadgeRepository badgeRepository;
     @Override
-    public void giveBadge(GiveBadgeRequest giveBadgeRequest, String senderName) {
+    public boolean giveBadge(GiveBadgeRequest giveBadgeRequest, String senderName) {
         Badge badge = badgeRepository.findById(giveBadgeRequest.getBadgeId()).get();
         Member receiver = memberRepository.findById(giveBadgeRequest.getReceiverId()).get();
         Member sender = memberRepository.findByEmail(senderName).get();
+        if (gaveSameCertainly(sender, receiver, badge)) return false;
         attachRepository.save(new Attach(badge, sender, receiver));
+        return true;
+    }
+
+    private boolean gaveSameCertainly(Member sender, Member receiver, Badge badge) {
+        LocalDateTime twentyFourHoursAgo = LocalDateTime.now().minusHours(24);
+        return attachRepository.existsBySenderIdAndReceiverIdAndBadgeIdAndCreatedAtAfter(sender, receiver, badge, twentyFourHoursAgo);
     }
 }
