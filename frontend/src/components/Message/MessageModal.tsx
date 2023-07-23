b38d33f0ef24fc4c765e785a50f98797409aec0e
이시영 <swy05123@gmail.com>
2023-07-23 19:22:32 +0900

    feat: 메신저 수정 S09P12B106-120

--- frontend/src/components/Message/MessageModal.tsx
+++ frontend/src/components/Message/MessageModal.tsx
@@ -1,87 +1,147 @@
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useState, ChangeEvent, useEffect } from "react";
 import styled from "styled-components";
-import ExitBox from "../../assets/Messenger/ExitBox.png"
+import ExitBox from "../../assets/Messenger/ExitBox.png";
+import ProfileImg from "../../assets/Messenger/profile.png";
+import SendArrow from "../../assets/Messenger/SendArrow.png";
 
 const ModalContainer = styled.div`
-    position: fixed;
-    left: 0;
-    top:0;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: fixed;
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
 `;
 
 const DialogBox = styled.dialog`
-    width: 360px;
-    height: 400px;
-    padding: 7px;
-    display: flex;
-    flex-direction: column;
-    font-weight: bold;
-    /* align-items: center; */
-    border: none;
-    border-radius: 10px;
-    box-shadow: 0 0 30px rgba(30, 30, 30, 0.185);
-    box-sizing: border-box;
-    background-color: white;
-    z-index: 10000;
+  width: 360px;
+  height: 400px;
+  padding: 7px;
+  display: flex;
+  flex-direction: column;
+  font-weight: bold;
+  /* align-items: center; */
+  border: none;
+  border-radius: 10px;
+  box-shadow: 0 0 30px rgba(30, 30, 30, 0.185);
+  box-sizing: border-box;
+  background-color: white;
+  z-index: 10000;
 `;
 
 const Backdrop = styled.div`
-    width: 100vw;
-    height: 100vh;
-    position: fixed;
-    top: 0;
-    z-index: 9999;
-    background-color: rgba(0, 0, 0, 0.2);
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  z-index: 9999;
+  background-color: rgba(0, 0, 0, 0.2);
 `;
 
 const ExitImg = styled.img`
-    position: absolute;
-    right: 10px;
-    top: 9px;
-    width: 20px;
-    height: 20px;
+  position: absolute;
+  right: 10px;
+  top: 9px;
+  width: 20px;
+  height: 20px;
 `;
 
 interface ModalDefaultType {
-    onClickToggleModal: () => void;
+  onClickToggleModal: () => void;
 }
 
-const MessageModal = ({ onClickToggleModal, children }: PropsWithChildren<ModalDefaultType>) => {
-    return (
-        <ModalContainer>
-            <DialogBox>
-                친구찾기
-                <ExitImg src={ExitBox} onClick={(e: React.MouseEvent) => {
-                    e.preventDefault();
-                    if (onClickToggleModal) {
-                        onClickToggleModal();
-                    }
-                }}/>
-                <form>
-                    <input type="text"></input>
-                </form>
-                {children}
-                {
-                    // 여기에 모달 친구추가 컴포넌트 만들어야..
-                    // 이미지, 이름 props로 전달 클릭 시 이벤트...
-                    // messagepage에서 props로 함수도 받아서 있는 방인지 확인하고
-                    // 해당 방으로 옮기거나 새로운 방 개설..
-                }
-            </DialogBox>
-            <Backdrop
-                onClick={(e: React.MouseEvent) => {
-                    e.preventDefault();
-                    if (onClickToggleModal) {
-                        onClickToggleModal();
-                    }
-                }}
-            />
-        </ModalContainer>
-    );
+interface Friend {
+  id: number;
+  name: string;
+  img: string;
 }
+
+//이름 다 다른가?
+const FriendsList = [
+  { id: 11, name: "aaa", img: ProfileImg },
+  { id: 12, name: "aaab", img:  ProfileImg },
+  { id: 13, name: "bbc", img:  ProfileImg },
+  { id: 14, name: "qweqe", img:  ProfileImg },
+  { id: 15, name: "zxzc", img:  ProfileImg },
+];
+
+const MessageModal = ({ onClickToggleModal, children }: PropsWithChildren<ModalDefaultType>) => {
+  const [searchForm, setSearch] = useState("");
+  const [curList, setCurList] = useState<Friend[]>([]);
+
+  //서버와 통신해서 해당 사용자의 친구목록 전부 표시 (이후 전역에 저장해 관리)
+  //FriendsList
+  useEffect(() => {
+    setCurList([...FriendsList]);
+  }, []);
+
+  const handleSearchForm = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const filterList = FriendsList.filter((p) => {
+    return p.name.toLocaleLowerCase().includes(searchForm.toLocaleLowerCase());
+  });
+
+  const onClickOpenNewRoom = (name: string) =>{
+    console.log(name);
+    alert(name+"과의 대화방 열기. 복잡해서 리코일 적용 이후로 방 열기 구현 예정");
+    //props로 전달해보려 했는데 모달에 함수 여러개 전달하려니까 안됨
+  }
+
+
+  return (
+    <ModalContainer>
+      <DialogBox>
+        <div>
+          친구찾기
+          <ExitImg
+            src={ExitBox}
+            onClick={(e: React.MouseEvent) => {
+              e.preventDefault();
+              if (onClickToggleModal) {
+                onClickToggleModal();
+              }
+            }}
+          />
+        </div>
+
+        <form>
+          <input
+            className="input"
+            type="text"
+            onChange={handleSearchForm}
+            value={searchForm}
+          ></input>
+        </form>
+        {children}
+
+        {
+          // 여기에 모달 친구추가 컴포넌트 만들어야..
+          // 이미지, 이름 props로 전달 클릭 시 이벤트...
+          // messagepage에서 props로 함수도 받아서 있는 방인지 확인하고
+          // 해당 방으로 옮기거나 새로운 방 개설..
+        }
+        {filterList.map((friend) => (
+          <div key={friend.id}>
+            <img src={friend.img} />
+            <span>{friend.name}</span>
+            <img src={SendArrow} onClick={()=>onClickOpenNewRoom(friend.name)}/>
+          </div>
+        ))}
+      </DialogBox>
+      <Backdrop
+        onClick={(e: React.MouseEvent) => {
+          e.preventDefault();
+          if (onClickToggleModal) {
+            onClickToggleModal();
+          }
+        }}
+      />
+    </ModalContainer>
+  );
+};
 export default MessageModal;
--- frontend/src/components/Message/MessageRoom.tsx
+++ frontend/src/components/Message/MessageRoom.tsx
@@ -88,9 +88,9 @@ const Input = styled.input`
 `;
 
 const Send = styled.img`
-position: absolute;
-right: 18px;
-bottom: 18px;
+  position: absolute;
+  right: 16px;
+  bottom: 16px;
 `;
 
 type Props = {
@@ -107,72 +107,66 @@ interface Post {
   date: string;
 }
 
-//입력 배열 잘못 만들어서 역순으로 되어있다고 가정
 const tmp = [
   {
-    chatId: 20,
+    chatId: 0,
+    id: 1,
+    member: "이시영0",
+    content: "뭐긴 뭐야1",
+    time: "10:9:21",
+    date: "2023-07-21",
+  },
+  {
+    chatId: 1,
     id: 1,
     member: "이시영1",
-    content: "아니 뭐야20",
-    time: "10:24:21",
+    content: "뭐긴 뭐야1",
+    time: "10:10:21",
     date: "2023-07-21",
   },
+  { chatId: 2, id: 1, member: "me", content: ";;;; 뭐야2", time: "10:11:21", date: "2023-07-21" },
+  { chatId: 3, id: 1, member: "me", content: "ㅎㅎㅎ 뭐야3", time: "10:11:21", date: "2023-07-21" },
   {
-    chatId: 19,
+    chatId: 4,
     id: 1,
     member: "이시영1",
-    content: "진짜 뭐야19",
-    time: "10:23:21",
+    content: "그냥 뭐야4",
+    time: "10:11:21",
     date: "2023-07-21",
   },
   {
-    chatId: 18,
+    chatId: 5,
     id: 1,
     member: "이시영1",
-    content: "=정말 뭐야18",
-    time: "10:22:21",
+    content: "왜;; 뭐야5",
+    time: "10:12:21",
     date: "2023-07-21",
   },
+  { chatId: 6, id: 1, member: "me", content: "뭐야뭐야6", time: "10:12:21", date: "2023-07-21" },
+
   {
-    chatId: 17,
+    chatId: 7,
     id: 1,
     member: "이시영1",
-    content: "ㄹㅇ 뭐야17",
-    time: "10:21:21",
+    content: "ㄹㅇ 뭐야7",
+    time: "10:13:21",
     date: "2023-07-21",
   },
-  { chatId: 16, id: 1, member: "me", content: "뭐야뭐야16", time: "10:20:21", date: "2023-07-21" },
   {
-    chatId: 15,
+    chatId: 8,
     id: 1,
     member: "이시영1",
-    content: "왜;; 뭐야15",
-    time: "10:20:21",
+    content: "=정말 뭐야8",
+    time: "10:14:21",
     date: "2023-07-21",
   },
+
   {
-    chatId: 14,
+    chatId: 9,
     id: 1,
     member: "이시영1",
-    content: "그냥 뭐야14",
-    time: "10:19:21",
-    date: "2023-07-21",
-  },
-  {
-    chatId: 13,
-    id: 1,
-    member: "me",
-    content: "ㅎㅎㅎ 뭐야13",
-    time: "10:18:21",
-    date: "2023-07-21",
-  },
-  { chatId: 12, id: 1, member: "me", content: ";;;; 뭐야12", time: "10:17:21", date: "2023-07-21" },
-  {
-    chatId: 11,
-    id: 1,
-    member: "me",
-    content: "zzzzzzzㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋzzzzzzz뭐야11",
-    time: "10:17:21",
+    content: "진짜 뭐야9",
+    time: "10:15:21",
     date: "2023-07-21",
   },
   {
@@ -184,62 +178,69 @@ const tmp = [
     date: "2023-07-21",
   },
   {
-    chatId: 9,
+    chatId: 11,
     id: 1,
-    member: "이시영1",
-    content: "진짜 뭐야9",
-    time: "10:15:21",
+    member: "me",
+    content: "zzzzzzzㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋzzzzzzz뭐야11",
+    time: "10:17:21",
+    date: "2023-07-21",
+  },
+  { chatId: 12, id: 1, member: "me", content: ";;;; 뭐야12", time: "10:17:21", date: "2023-07-21" },
+  {
+    chatId: 13,
+    id: 1,
+    member: "me",
+    content: "ㅎㅎㅎ 뭐야13",
+    time: "10:18:21",
     date: "2023-07-21",
   },
   {
-    chatId: 8,
+    chatId: 14,
     id: 1,
     member: "이시영1",
-    content: "=정말 뭐야8",
-    time: "10:14:21",
+    content: "그냥 뭐야14",
+    time: "10:19:21",
     date: "2023-07-21",
   },
   {
-    chatId: 7,
+    chatId: 15,
     id: 1,
     member: "이시영1",
-    content: "ㄹㅇ 뭐야7",
-    time: "10:13:21",
+    content: "왜;; 뭐야15",
+    time: "10:20:21",
     date: "2023-07-21",
   },
-  { chatId: 6, id: 1, member: "me", content: "뭐야뭐야6", time: "10:12:21", date: "2023-07-21" },
+  { chatId: 16, id: 1, member: "me", content: "뭐야뭐야16", time: "10:20:21", date: "2023-07-21" },
   {
-    chatId: 5,
+    chatId: 17,
     id: 1,
     member: "이시영1",
-    content: "왜;; 뭐야5",
-    time: "10:12:21",
-    date: "2023-07-21",
-  },
-  {
-    chatId: 4,
-    id: 1,
-    member: "이시영1",
-    content: "그냥 뭐야4",
-    time: "10:11:21",
-    date: "2023-07-21",
-  },
-  { chatId: 3, id: 1, member: "me", content: "ㅎㅎㅎ 뭐야3", time: "10:11:21", date: "2023-07-21" },
-  { chatId: 2, id: 1, member: "me", content: ";;;; 뭐야2", time: "10:11:21", date: "2023-07-21" },
-  {
-    chatId: 1,
-    id: 1,
-    member: "이시영1",
-    content: "뭐긴 뭐야1",
-    time: "10:10:21",
+    content: "ㄹㅇ 뭐야17",
+    time: "10:21:21",
     date: "2023-07-21",
   },
   {
-    chatId: 0,
+    chatId: 18,
     id: 1,
-    member: "이시영0",
-    content: "뭐긴 뭐야1",
-    time: "10:9:21",
+    member: "이시영1",
+    content: "=정말 뭐야18",
+    time: "10:22:21",
+    date: "2023-07-21",
+  },
+  {
+    chatId: 19,
+    id: 1,
+    member: "이시영1",
+    content: "진짜 뭐야19",
+    time: "10:23:21",
+    date: "2023-07-21",
+  },
+  {
+    chatId: 20,
+    id: 1,
+    member: "이시영1",
+    content: "아니 뭐야20",
+    time: "10:24:21",
     date: "2023-07-21",
   },
 ];
@@ -260,7 +261,7 @@ const MessageRoom: React.FC<Props> = ({ onClickSetRoom, openRoomId }) => {
   };
 
   const onClickSend = () => {
-    if(message === "") return;
+    if (message === "") return;
     console.log(posts);
     let curChatId = 0;
     if (posts.length > 0) {
@@ -283,8 +284,7 @@ const MessageRoom: React.FC<Props> = ({ onClickSetRoom, openRoomId }) => {
   };
 
   useEffect(() => {
-    //입력 배열 잘못 만들어서 역순으로 되어있다고 가정
-    setPosts([...tmp].slice(0).reverse());
+    setPosts([...tmp]);
     console.log(posts);
   }, []);
 
@@ -340,9 +340,7 @@ const MessageRoom: React.FC<Props> = ({ onClickSetRoom, openRoomId }) => {
           )
         )}
 
-        <div
-          ref={startRef}
-        ></div>
+        <div ref={startRef}></div>
       </InfinityScroll>
       <MessegeBodyDiv>
         <div>
