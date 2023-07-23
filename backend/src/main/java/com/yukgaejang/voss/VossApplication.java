210aa20af2f7b1f9254d4e005bc3ffe8ff925f16
210-reverof <sheltonwon@gmail.com>
2023-07-23 20:20:02 +0900

    feat: EnableJpaAuditing 설정 S09P12B106-59

--- backend/src/main/java/com/yukgaejang/voss/VossApplication.java
+++ backend/src/main/java/com/yukgaejang/voss/VossApplication.java
@@ -2,8 +2,10 @@ package com.yukgaejang.voss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class VossApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VossApplication.class, args);
