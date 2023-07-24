67df446e81020f6c03617d8ee3034b8d0e67a136
이승종 <maeugel_rosh@naver.com>
2023-07-24 15:44:59 +0900

    fix: MeetRequestDto 삭제 S09P12B106-156

--- backend/src/main/java/com/yukgaejang/voss/domain/meet/service/MeetService.java
+++ backend/src/main/java/com/yukgaejang/voss/domain/meet/service/MeetService.java
@@ -1,10 +1,9 @@
 package com.yukgaejang.voss.domain.meet.service;
 
 import com.yukgaejang.voss.domain.meet.service.dto.response.MeetResponseDto;
-import com.yukgaejang.voss.domain.meet.service.dto.request.MeetRequestDto;
 import org.springframework.data.domain.Page;
 
 public interface MeetService {
 
-    Page<MeetResponseDto> list(MeetRequestDto meetRequestDto);
+    Page<MeetResponseDto> list(int page, int limit);
 }
