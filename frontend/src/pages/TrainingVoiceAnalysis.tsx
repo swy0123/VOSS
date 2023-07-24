ae19746ccd07f80e03fd97f94e2ba4bfb7a6692b
hyunwoo <gusehd3279@gmail.com>
2023-07-24 22:52:22 +0900

    init: 녹음버튼 시작 S09P12B106-255

--- frontend/src/pages/TrainingVoiceAnalysis.tsx
+++ frontend/src/pages/TrainingVoiceAnalysis.tsx
@@ -4,10 +4,16 @@ import Header from '../components/Header/Header';
 import Script from '../components/Training/Script';
 import Recording from '../components/Training/Recording';
 import AnalysisButton from '../components/Training/AnalysisButton';
+import Messenger from '../components/Message/Messenger';
+import RecordButton from '../components/Training/RecordButton';
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+const UpSection = styled.div`
+  display: flex;
 `
 const LeftSection = styled.div`
   display: flex;
@@ -24,14 +30,19 @@ function TrainingVoiceAnalysis() {
     <BackGroundImg>
       <Header></Header>
       <Container>
-        <LeftSection>
-          <Script></Script>
-        </LeftSection>
-        <RightSection>
-          <Recording></Recording>
-          <AnalysisButton></AnalysisButton>
-        </RightSection>
+        <UpSection>
+          <LeftSection>
+            <Script></Script>
+          </LeftSection>
+          <RightSection>
+            <Recording></Recording>
+            <AnalysisButton></AnalysisButton>
+          </RightSection>
+        </UpSection>
+        <RecordButton></RecordButton>
       </Container>
+
+      <Messenger/>
     </BackGroundImg>
   )
 }
