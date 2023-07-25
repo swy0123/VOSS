ae0ea78214167379d755e24c8957bef30d8d1ced
이시영 <swy05123@gmail.com>
2023-07-25 13:05:56 +0900

    feat: 화상회의 목록 게시판 틀 생성 S09P12B106-181

---
+++ frontend/src/components/Meeting/MeetingList.tsx
@@ -0,0 +1,20 @@
+import { styled } from "styled-components";
+
+const ListBox = styled.div`
+    height: 90%;
+    background-color: gray;
+
+`;
+
+function MeetingList(){
+
+    return (
+        <ListBox>
+            목록
+        </ListBox>
+    )
+
+
+}
+
+export default MeetingList;
