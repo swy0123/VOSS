f495e61781c3cf2a000db256fa9adab051730f95
이승종 <maeugel_rosh@naver.com>
2023-07-25 10:09:35 +0900

    feat: meetId로 현재 참여중인 MeetJoins 가져오기 S09P12B106-158

--- backend/src/main/java/com/yukgaejang/voss/domain/meet/repository/MeetJoinRepository.java
+++ backend/src/main/java/com/yukgaejang/voss/domain/meet/repository/MeetJoinRepository.java
@@ -3,5 +3,10 @@ package com.yukgaejang.voss.domain.meet.repository;
 import com.yukgaejang.voss.domain.meet.repository.entity.MeetJoin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MeetJoinRepository extends JpaRepository<MeetJoin, Long> {
+
+    List<MeetJoin> findByMeetId(Long meetId);
 }
