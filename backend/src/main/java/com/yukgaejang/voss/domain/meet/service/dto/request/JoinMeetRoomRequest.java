5eb04b4a7b707c6238b2327d20d90f2221371417
이승종 <maeugel_rosh@naver.com>
2023-07-25 13:57:08 +0900

    feat: 미팅방 입장 request, response 생성 S09P12B106-158

---
+++ backend/src/main/java/com/yukgaejang/voss/domain/meet/service/dto/request/JoinMeetRoomRequest.java
@@ -0,0 +1,11 @@
+package com.yukgaejang.voss.domain.meet.service.dto.request;
+
+import lombok.Data;
+
+@Data
+public class JoinMeetRoomRequest {
+    private String email;
+    private String password;
+    private Long meetId;
+
+}
