c0d40146adbcdd58e37c82347f69ff3d6a4b9db5
김수연 <agew0423@daum.net>
2023-07-25 15:41:58 +0900

    feat: 게시글 상세 조회 S09P12B106-204

--- backend/src/main/java/com/yukgaejang/voss/domain/freeboard/repository/PostRepository.java
+++ backend/src/main/java/com/yukgaejang/voss/domain/freeboard/repository/PostRepository.java
@@ -2,9 +2,16 @@ package com.yukgaejang.voss.domain.freeboard.repository;
 
 import com.yukgaejang.voss.domain.freeboard.repository.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Post" +
+            "set title=:title, content=:content" +
+            "where id=:id")
+    Post update(Long id, String title, String content);
 }
