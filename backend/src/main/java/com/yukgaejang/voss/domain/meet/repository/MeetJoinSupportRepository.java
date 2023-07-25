1355ba7bd6be7ddc23c921a049fa2ca4da632d9d
이승종 <maeugel_rosh@naver.com>
2023-07-25 13:58:52 +0900

    feat: MeetJoin 구현 S09P12B106-158

---
+++ backend/src/main/java/com/yukgaejang/voss/domain/meet/repository/MeetJoinSupportRepository.java
@@ -0,0 +1,10 @@
+package com.yukgaejang.voss.domain.meet.repository;
+
+import com.yukgaejang.voss.domain.meet.repository.entity.MeetJoin;
+
+import java.util.List;
+
+public interface MeetJoinSupportRepository {
+
+    List<MeetJoin> findByMeetId(Long meetId);
+}
