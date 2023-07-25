1249d3fb5ef765e0d690c0cb3fd5e8ed4f0f7337
정현우 <gusehd502@naver.com>
2023-07-25 10:21:46 +0900

    design: 발음교정 디자인 완성 S09P12B106-234

---
+++ frontend/src/components/Accent/AccentResult.tsx
@@ -0,0 +1,17 @@
+import { styled } from 'styled-components';
+
+const ResultBox = styled.div`
+  background-color: #2E2E2E;
+  border-radius: 8px;
+  margin-top: 10px;
+  height: 200px;
+`
+
+function AccentResult () {
+  return (
+    <ResultBox>
+
+    </ResultBox>
+  )
+}
+export default AccentResult
