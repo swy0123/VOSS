6e86cfaa5326e48350abd02591bf344c9bab6dfe
이승종 <maeugel_rosh@naver.com>
2023-07-25 13:58:23 +0900

    feat: 미팅방 조회 구현 S09P12B106-158

--- backend/src/main/java/com/yukgaejang/voss/domain/meet/repository/MeetSupportRepository.java
+++ backend/src/main/java/com/yukgaejang/voss/domain/meet/repository/MeetSupportRepository.java
@@ -1,4 +1,11 @@
 package com.yukgaejang.voss.domain.meet.repository;
 
+import com.yukgaejang.voss.domain.meet.repository.entity.Meet;
+
+import java.util.Optional;
+
+
 public interface MeetSupportRepository {
+
+    Optional<Meet> findByMeetId(Long meetId);
 }
