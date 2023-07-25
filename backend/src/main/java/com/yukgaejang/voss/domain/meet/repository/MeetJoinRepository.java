b37a5326559ad3ad60a876a83241126a3de5d0af
이승종 <maeugel_rosh@naver.com>
2023-07-25 13:58:52 +0900

    feat: MeetJoin 구현 S09P12B106-158

--- backend/src/main/java/com/yukgaejang/voss/domain/meet/repository/MeetJoinRepository.java
+++ backend/src/main/java/com/yukgaejang/voss/domain/meet/repository/MeetJoinRepository.java
@@ -3,10 +3,7 @@ package com.yukgaejang.voss.domain.meet.repository;
 import com.yukgaejang.voss.domain.meet.repository.entity.MeetJoin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
 
-public interface MeetJoinRepository extends JpaRepository<MeetJoin, Long> {
+public interface MeetJoinRepository extends JpaRepository<MeetJoin, Long>, MeetJoinSupportRepository {
 
-    List<MeetJoin> findByMeetId(Long meetId);
 }
