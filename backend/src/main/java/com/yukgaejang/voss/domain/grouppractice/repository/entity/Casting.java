2ef8bfe4f0b8b0ebfa6e9b3cda2eef7d34ca9ca5
이승종 <maeugel_rosh@naver.com>
2023-07-26 06:41:43 +0900

    fix: Casting 엔티티 수정 S09P12B106-173

--- backend/src/main/java/com/yukgaejang/voss/domain/grouppractice/repository/entity/Casting.java
+++ backend/src/main/java/com/yukgaejang/voss/domain/grouppractice/repository/entity/Casting.java
@@ -2,15 +2,20 @@ package com.yukgaejang.voss.domain.grouppractice.repository.entity;
 
 import com.yukgaejang.voss.domain.meet.repository.entity.MeetJoin;
 import com.yukgaejang.voss.domain.practice.repository.entity.ScriptLine;
+import com.yukgaejang.voss.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Casting {
+@ToString
+public class Casting extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -18,10 +23,10 @@ public class Casting {
     @ManyToOne(fetch = FetchType.LAZY)
     private MeetJoin meetJoin;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    private ScriptLine scriptLine;
+    @OneToMany
+    private List<ScriptLine> scriptLine;
 
-    public Casting(MeetJoin meetJoin, ScriptLine scriptLine) {
+    public Casting(MeetJoin meetJoin, List<ScriptLine> scriptLine) {
         this.meetJoin = meetJoin;
         this.scriptLine = scriptLine;
     }
