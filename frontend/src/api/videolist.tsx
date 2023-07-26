ee64ca863c832dbb28e9edee81482318a20522a1
정현우 <gusehd502@naver.com>
2023-07-26 16:59:58 +0900

    test: axios테스트 성공 S09P12B106-246

--- frontend/src/api/videolist.tsx
+++ frontend/src/api/videolist.tsx
@@ -28,13 +28,12 @@ interface ScriptData {
 export const getVideos = async () => {
   const config = {
     method: 'GET',
-    url: 'http://i9b106.p.ssafy.io:8080/practice/dub/1',
-    maxBodyLength: Infinity,
-    headers: {
-      Authorization: 'Bearer z'
+    url: 'https://i9b106.p.ssafy.io:8080/practice/dub/1',
+    headers: { 
+      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTY5MDM1NjMxMSwiZW1haWwiOiJuZXdAbmF2ZXIuY29tIn0.vX4sdHDmLfnwdJ3Fnv5dqe2U4qP1ezF4xOUVlmYtFr1apKjwFYw_G4bJtybhrfgyt-qKUo1OqUl3gfJkFL_8-Q', 
     }
   };
-
+  console.log("여기까지는 오나??")
   try{
     const response:AxiosResponse<ScriptData> = await axios.request(config)
     console.log(response)
@@ -43,3 +42,25 @@ export const getVideos = async () => {
     console.log(error)
   }
 }
+
+
+export const test = async () => {
+  const config2 = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: 'https://i9b106.p.ssafy.io:8080/auth/jwt-test',
+    headers: { 
+      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTY5MDM1NjMxMSwiZW1haWwiOiJuZXdAbmF2ZXIuY29tIn0.vX4sdHDmLfnwdJ3Fnv5dqe2U4qP1ezF4xOUVlmYtFr1apKjwFYw_G4bJtybhrfgyt-qKUo1OqUl3gfJkFL_8-Q', 
+    }
+  };
+  try{
+    const response = await axios.request(config2)
+    console.log(response)
+  }
+  catch (error){
+    console.log("catch야??")
+    console.log(error)
+
+  }
+}
+
