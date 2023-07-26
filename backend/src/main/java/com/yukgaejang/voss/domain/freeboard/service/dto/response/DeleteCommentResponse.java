638c6bc19ca064c5625a13534d5ed7cd602c4d3b
suyeonsu <agew0423@daum.net>
2023-07-26 10:10:51 +0900

    feat: 댓글 삭제 S09P12B106-210

---
+++ backend/src/main/java/com/yukgaejang/voss/domain/freeboard/service/dto/response/DeleteCommentResponse.java
@@ -0,0 +1,9 @@
+package com.yukgaejang.voss.domain.freeboard.service.dto.response;
+
+public class DeleteCommentResponse {
+    private boolean isSuccess;
+
+    public DeleteCommentResponse(boolean isSuccess) {
+        this.isSuccess = isSuccess;
+    }
+}
