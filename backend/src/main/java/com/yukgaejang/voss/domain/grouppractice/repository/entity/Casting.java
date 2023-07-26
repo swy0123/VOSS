ce9ece2a2b0c5b4fc5fea59b09f0c52b1256b701
210-reverof <sheltonwon@gmail.com>
2023-07-26 16:49:44 +0900

    fix: 충돌

--- backend/src/main/java/com/yukgaejang/voss/domain/grouppractice/repository/entity/Casting.java
+++ backend/src/main/java/com/yukgaejang/voss/domain/grouppractice/repository/entity/Casting.java
@@ -2,20 +2,30 @@ package com.yukgaejang.voss.domain.grouppractice.repository.entity;
 
 import com.yukgaejang.voss.domain.meet.repository.entity.MeetJoin;
 import com.yukgaejang.voss.domain.practice.repository.entity.ScriptLine;
+<<<<<<< HEAD
 import com.yukgaejang.voss.global.entity.BaseEntity;
+=======
+>>>>>>> c203284 (feat: Casing Entity 생성 S09P12B106-173)
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+<<<<<<< HEAD
 import lombok.ToString;
 
 import java.util.List;
+=======
+>>>>>>> c203284 (feat: Casing Entity 생성 S09P12B106-173)
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+<<<<<<< HEAD
 @ToString
 public class Casting extends BaseEntity {
+=======
+public class Casting {
+>>>>>>> c203284 (feat: Casing Entity 생성 S09P12B106-173)
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,10 +33,17 @@ public class Casting extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     private MeetJoin meetJoin;
 
+<<<<<<< HEAD
     @OneToMany
     private List<ScriptLine> scriptLine;
 
     public Casting(MeetJoin meetJoin, List<ScriptLine> scriptLine) {
+=======
+    @OneToOne(fetch = FetchType.LAZY)
+    private ScriptLine scriptLine;
+
+    public Casting(MeetJoin meetJoin, ScriptLine scriptLine) {
+>>>>>>> c203284 (feat: Casing Entity 생성 S09P12B106-173)
         this.meetJoin = meetJoin;
         this.scriptLine = scriptLine;
     }
