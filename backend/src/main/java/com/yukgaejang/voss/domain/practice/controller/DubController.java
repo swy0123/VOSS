043c56bfd9e2b6eaf4d2e643f8b73104e5e22c06
210-reverof <sheltonwon@gmail.com>
2023-07-26 21:54:34 +0900

    feat: DubController, dto 완성 S09P12B106-222

--- backend/src/main/java/com/yukgaejang/voss/domain/practice/controller/DubController.java
+++ backend/src/main/java/com/yukgaejang/voss/domain/practice/controller/DubController.java
@@ -1,6 +1,7 @@
 package com.yukgaejang.voss.domain.practice.controller;
 
 import com.yukgaejang.voss.domain.practice.serivce.DubService;
+import com.yukgaejang.voss.domain.practice.serivce.dto.response.ScriptListResponse;
 import com.yukgaejang.voss.domain.practice.serivce.dto.response.ViewScriptResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequestMapping("/practice/dub")
 @RequiredArgsConstructor
@@ -20,5 +23,11 @@ public class DubController {
     public ResponseEntity<ViewScriptResponse> viewScript(@PathVariable("scriptId") Long id) {
         ViewScriptResponse viewScriptResponse = dubService.viewScript(id);
         return ResponseEntity.ok(viewScriptResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ScriptListResponse>> viewScriptList() {
+        List<ScriptListResponse> scriptList = dubService.getScriptList();
+        return ResponseEntity.ok(scriptList);
     }
 }
---
+++ backend/src/main/java/com/yukgaejang/voss/domain/practice/serivce/dto/response/ScriptListResponse.java
@@ -0,0 +1,35 @@
+package com.yukgaejang.voss.domain.practice.serivce.dto.response;
+
+import com.yukgaejang.voss.domain.practice.repository.entity.Category;
+import com.yukgaejang.voss.domain.practice.repository.entity.Script;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class ScriptListResponse {
+    private Long id;
+
+    private String category;
+
+    private String title;
+
+    private Long durationInSec;
+
+    private String videoUrl;
+
+    private Integer roleCnt;
+
+    public ScriptListResponse(Script script) {
+        this.id = script.getId();
+        this.category = script.getCategory().name();
+        this.title = script.getTitle();
+        this.durationInSec = script.getDurationInSec();
+        this.videoUrl = script.getVideoUrl();
+        this.roleCnt = script.getRoleCnt();
+    }
+}
