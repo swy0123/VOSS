e937a54a78e64fd3b012f2709b248e842e8d5bc5
이승종 <maeugel_rosh@naver.com>
2023-07-27 21:59:29 +0900

    feat: 미팅룸 리스트 요청 추가 S09P12B106-167

---
+++ backend/src/main/java/com/yukgaejang/voss/domain/meet/service/dto/request/MeetSearchCondition.java
@@ -0,0 +1,14 @@
+package com.yukgaejang.voss.domain.meet.service.dto.request;
+
+import com.yukgaejang.voss.domain.meet.repository.entity.Category;
+import lombok.Data;
+
+@Data
+public class MeetSearchCondition {
+    // 제목, 카테고리
+    private String title;
+    private Category category;
+    private int page;
+    private int limit;
+
+}
