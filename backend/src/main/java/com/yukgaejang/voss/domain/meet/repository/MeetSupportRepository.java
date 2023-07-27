207fbdc0580c23944ec55286bf09ad544adf6d2d
이승종 <maeugel_rosh@naver.com>
2023-07-27 22:03:19 +0900

    feat: 미팅룸 리스트 컨트롤러 S09P12B106-167

--- backend/src/main/java/com/yukgaejang/voss/domain/meet/repository/MeetSupportRepository.java
+++ backend/src/main/java/com/yukgaejang/voss/domain/meet/repository/MeetSupportRepository.java
@@ -1,8 +1,11 @@
 package com.yukgaejang.voss.domain.meet.repository;
 
 import com.yukgaejang.voss.domain.meet.repository.entity.Meet;
+import com.yukgaejang.voss.domain.meet.service.dto.request.MeetSearchCondition;
 import com.yukgaejang.voss.domain.meet.service.dto.request.SelectScriptRequest;
 import com.yukgaejang.voss.domain.practice.repository.entity.Script;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
@@ -14,4 +17,6 @@ public interface MeetSupportRepository {
     long setScript(SelectScriptRequest selectScriptRequest, Script script);
 
     void leaveMeetRoom(Long meetId);
+
+    Page<Meet> getMeetList(MeetSearchCondition condition, Pageable pageable);
 }
--- backend/src/main/java/com/yukgaejang/voss/domain/meet/repository/MeetSupportRepositoryImpl.java
+++ backend/src/main/java/com/yukgaejang/voss/domain/meet/repository/MeetSupportRepositoryImpl.java
@@ -1,14 +1,24 @@
 package com.yukgaejang.voss.domain.meet.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.yukgaejang.voss.domain.meet.repository.entity.Category;
 import com.yukgaejang.voss.domain.meet.repository.entity.Meet;
+import com.yukgaejang.voss.domain.meet.repository.entity.QMeetJoin;
+import com.yukgaejang.voss.domain.meet.service.dto.request.MeetSearchCondition;
 import com.yukgaejang.voss.domain.meet.service.dto.request.SelectScriptRequest;
 import com.yukgaejang.voss.domain.practice.repository.entity.Script;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.yukgaejang.voss.domain.meet.repository.entity.QMeet.*;
+import static com.yukgaejang.voss.domain.meet.repository.entity.QMeetJoin.*;
 
 @Repository
 public class MeetSupportRepositoryImpl implements MeetSupportRepository{
@@ -44,6 +54,41 @@ public class MeetSupportRepositoryImpl implements MeetSupportRepository{
                 .set(meet.isDeleted, true)
                 .where(meet.id.eq(meetId))
                 .execute();
+    }
+
+    @Override
+    public Page<Meet> getMeetList(MeetSearchCondition condition, Pageable pageable) {
+        List<Meet> content = queryFactory
+                .selectDistinct(meet)
+                .from(meet)
+                .join(meet.meetJoins, meetJoin).fetchJoin()
+                .where(
+                        titleContains(condition.getTitle()),
+                        categoryEq(condition.getCategory()),
+                        meet.isDeleted.eq(false)
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+        JPAQuery<Long> countQuery = queryFactory
+                .select(meet.count())
+                .from(meet)
+                .join(meet.meetJoins, meetJoin).fetchJoin()
+                .where(
+                        titleContains(condition.getTitle()),
+                        categoryEq(condition.getCategory()),
+                        meet.isDeleted.eq(false)
+                );
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
+
+    }
+
+    private BooleanExpression titleContains(String title) {
+        return title == null ? null : meet.title.contains(title);
+    }
+
+    private BooleanExpression categoryEq(Category category) {
+        return category == null ? null : meet.category.eq(category);
     }
 
 
