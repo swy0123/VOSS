da69875b2fe0fdc9e0ec63b2b6cf05ef20b7d9c5
이시영 <swy05123@gmail.com>
2023-07-27 10:42:30 +0900

    feat: 에러 수

--- frontend/src/pages/Meeting/Meeting.tsx
+++ frontend/src/pages/Meeting/Meeting.tsx
@@ -1,14 +1,8 @@
 import { BackGroundImg } from '../../components/BackGroundImg';
 import { styled } from 'styled-components';
 import Messenger from '../../components/Message/Messenger';
-import Script from '../../components/DubbingRoom/Script';
-import Video from '../../components/DubbingRoom/Video';
 import { useParams } from 'react-router';
 import { Container, H1, LeftSection, RightSection } from './Meeting.style';
-<<<<<<< HEAD
-=======
-
->>>>>>> 6f6f43c (feat: style 코드 분리)
 
 interface MeetingData {
   index: number;
@@ -21,7 +15,7 @@ interface MeetingData {
 
 
 function Meeting() {
-  const { id } = useParams() as { id: string };
+  const {id} = useParams() as { id: string };
 
   //id를 통해 해당 화상회의방 api 호출
 
@@ -31,15 +25,10 @@ function Meeting() {
         <H1>{id}</H1>
 
         <LeftSection>
-<<<<<<< HEAD
-=======
-          <Video></Video>
->>>>>>> 6f6f43c (feat: style 코드 분리)
           
         </LeftSection>
 
         <RightSection>
-          <Video></Video>
         </RightSection>
 
       </Container>
