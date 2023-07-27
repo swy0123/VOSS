4beb260d508f3ad87a05ac8bf17a140210e7b91b
이승종 <maeugel_rosh@naver.com>
2023-07-27 23:45:27 +0900

    feat: 미팅참여멤버 응답 S09P12B106-182

--- backend/src/main/java/com/yukgaejang/voss/domain/meet/service/dto/response/getAllMeetJoinResponse.java
+++
@@ -1,9 +0,0 @@
-package com.yukgaejang.voss.domain.meet.service.dto.response;
-
-import lombok.Data;
-
-@Data
-public class getAllMeetJoinResponse {
-    private Long memberId;
-    private String nickname;
-}
