9b61d8ac182226f0afd3f1b8f6883d47905ae2b1
이승종 <maeugel_rosh@naver.com>
2023-07-29 02:58:32 +0900

    feat: Firebase Config 작성 S09P12B106-201

---
+++ backend/src/main/java/com/yukgaejang/voss/global/config/FirebaseConfig.java
@@ -0,0 +1,26 @@
+package com.yukgaejang.voss.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+
+@Configuration
+public class FirebaseConfig {
+    @PostConstruct
+    public void init() {
+        try {
+            FileInputStream serviceAccount = new FileInputStream("src/main/resources/external/serviceAccountKey.json");
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
