fcfbddaf89c433bbca303ab83e872fde30ce6fac
이승종 <maeugel_rosh@naver.com>
2023-07-29 19:35:59 +0900

    feat: WebSocket Config Handler 추가 S09P12B106-201

---
+++ backend/src/main/java/com/yukgaejang/voss/domain/messenger/websocket/WebSocketHandler.java
@@ -0,0 +1,31 @@
+package com.yukgaejang.voss.domain.messenger.websocket;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yukgaejang.voss.domain.messenger.service.MessengerService;
+import com.yukgaejang.voss.domain.messenger.service.dto.ChatMessageDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketHandler extends TextWebSocketHandler {
+    private final ObjectMapper objectMapper;
+    private final MessengerService messengerService;
+
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.info("{}", payload);
+        ChatMessageDto chatMessageDto = objectMapper.readValue(payload, ChatMessageDto.class);
+
+        ChatRoom chatRoom = messengerService.findRoomById(chatMessageDto.getChatId());
+        chatRoom.handlerAction(session, chatMessageDto, messengerService);
+
+    }
+}
---
+++ backend/src/main/java/com/yukgaejang/voss/global/config/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.yukgaejang.voss.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final WebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "ws/messenger").setAllowedOrigins("*");
+    }
+}
