f8cd0383954d32646ce34ec4aed5f12f49c19be0
정현우 <gusehd502@naver.com>
2023-07-31 09:33:04 +0900

    test: 수정 없음 S09P12B106-253

--- frontend/src/components/DubbingRoom/Video/Video.tsx
+++ frontend/src/components/DubbingRoom/Video/Video.tsx
@@ -1,8 +1,8 @@
 import { useRecoilState } from "recoil";
-import { Script, ScriptData } from "../../../type/type";
+import { Script } from "../../../type/type";
 import { Container, Display, Title } from "./Video.style";
-import { PlayChangebState, ScriptSelectState, videoState } from "../../../recoil/hw_atom";
-import { useEffect } from "react";
+import { PlayChangebState } from "../../../recoil/hw_atom";
+
 
 interface VideoProps {
   script : Script
@@ -10,8 +10,6 @@ interface VideoProps {
 
 function Video ({script}: VideoProps) {
   const [playChange, setPlayChange] = useRecoilState<number[]>(PlayChangebState)
-  const [isScriptSelect,setIsScriptSelect] = useRecoilState<boolean[]>(ScriptSelectState)
-  const [video, setVideo] = useRecoilState<ScriptData | null>(videoState)
 
   const onYouTubeIframeAPIReady = () => {
     const player = new YT.Player('player', {
@@ -21,46 +19,16 @@ function Video ({script}: VideoProps) {
       }
     });
   }
-
-  // setTimeout(() => {
-  //   event.target.mute();
-  // }, 3000);
-
+  
   const onPlayStateChange = (event) => {
     if (event.data == YT.PlayerState.PLAYING) {
-      let nowTime = Math.floor(event.target.getCurrentTime())
-      setPlayChange([1,nowTime])
-      console.log(nowTime)
-      console.log(isScriptSelect)
-      
-      video.lines.forEach((line,index)=> {
-        if(isScriptSelect[index]==false){
-          if(nowTime >= line.startSec && nowTime <= line.endSec){
-            event.target.mute()
-            setTimeout(() => {
-              event.target.unMute()
-              nowTime = line.endSec
-            },(line.endSec-nowTime)*1000)
-          }
-          else if(nowTime < line.startSec){
-            setTimeout(() => {
-              event.target.mute()
-            },(line.startSec-nowTime)*1000)
-
-            setTimeout(() => {
-              event.target.unMute()
-              nowTime = line.endSec
-            },(line.endSec-line.startSec)*1000)
-
-          }
-        }        
-      })
+      setPlayChange([1,Math.floor(event.target.getCurrentTime())])
     }
     else if (event.data == YT.PlayerState.PAUSED) {
       setPlayChange([2,Math.floor(event.target.getCurrentTime())])
     }
   }
-
+  
   // index.html에 CDN을 동적으로 추가해주는 과정이라 생각하자
   const tag = document.createElement('script');
   tag.src = 'https://www.youtube.com/iframe_api';
@@ -70,6 +38,7 @@ function Video ({script}: VideoProps) {
   }
   window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
 
+  // if(!roles || !script) { return }  
   return(
     <Container>
       <Title>{script.title}</Title>
