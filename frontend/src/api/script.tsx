3aaa582c61bcc95300f817219dbbc81d5cfe8954
hyunwoo <gusehd3279@gmail.com>
2023-08-04 01:33:41 +0900

    fix: 스크립트 요청 내용 수정 S09P12B106-358

--- frontend/src/api/script.tsx
+++ frontend/src/api/script.tsx
@@ -8,7 +8,7 @@ type AsyncFunction = (
 export const makeAnalysisScript: AsyncFunction = async(genderSelected,ageSelected) => {
   try{
     const response: AxiosResponse<string> = await privateApi.get<string>(
-      `/practice/diction/script?cmd=${ageSelected} ${genderSelected}에게 어울리는 말 60자 이내로 작성해줘`
+      `/practice/diction/script?cmd=${ageSelected} ${genderSelected} 성우지망생이 연습할 더빙 대사 60자 이내로 작성해줘`
     )
     return response.data
   }
@@ -20,7 +20,7 @@ export const makeAnalysisScript: AsyncFunction = async(genderSelected,ageSelecte
 export const makeAccentScript: AsyncFunction = async(categorySelected) => {
   try{
     const response: AxiosResponse<string> = await privateApi.get<string>(
-      `/practice/diction/script?cmd=${categorySelected}을 카테고리로 대사 60자 이내로 작성해줘`
+      `/practice/diction/script?cmd=성우지망생이 연습할 ${categorySelected} 카테고리의 대사 60자 이내로 작성해줘`
     )
     return response.data
   }
