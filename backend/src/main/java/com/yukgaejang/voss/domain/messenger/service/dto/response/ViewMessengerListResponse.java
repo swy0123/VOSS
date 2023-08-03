9f1930e81a8360693e664290a6cd7680d2984b6b
이승종 <maeugel_rosh@naver.com>
2023-08-03 12:49:34 +0900

    fix: memberId반환 추가 S09P12B106-348

--- backend/src/main/java/com/yukgaejang/voss/domain/messenger/service/dto/response/ViewMessengerListResponse.java
+++ backend/src/main/java/com/yukgaejang/voss/domain/messenger/service/dto/response/ViewMessengerListResponse.java
@@ -7,11 +7,13 @@ import lombok.Data;
 public class ViewMessengerListResponse {
     private String name;
     private Long chatId;
+    private Long memberId;
     private String sessionId;
 
     public ViewMessengerListResponse(Attend attend) {
         this.name = attend.getMember().getNickname();
         this.chatId = attend.getChat().getId();
+        this.memberId = attend.getMember().getId();
         this.sessionId = attend.getChat().getSession();
     }
 }
