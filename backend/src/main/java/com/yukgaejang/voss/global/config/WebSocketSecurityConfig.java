490973ae7db350777d3a7af5f31556a85bf47973
이원영 <sheltonwon@gmail.com>
2023-08-03 17:09:42 +0900

    feat: add ws config

--- backend/src/main/java/com/yukgaejang/voss/global/config/WebSocketSecurityConfig.java
+++ backend/src/main/java/com/yukgaejang/voss/global/config/WebSocketSecurityConfig.java
@@ -1,16 +1,14 @@
-package com.yukgaejang.voss.global.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
-import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
-
-@Configuration
-public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
-
-    @Override
-    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
-        messages.simpDestMatchers("/**").permitAll();
-//                .simpDestMatchers("/admin/**").hasRole("ADMIN").anyMessage()
-//                .authenticated();
-    }
-}
+//package com.yukgaejang.voss.global.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+//import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+//
+//@Configuration
+//public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+//
+//    @Override
+//    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+//        messages.simpDestMatchers("/**").permitAll();
+//    }
+//}
