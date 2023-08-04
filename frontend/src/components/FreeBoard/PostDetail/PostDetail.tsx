428a68ebedd73e9dd36c3c2c0c7ae5a5af852bb4
김준섭 <vacanter@naver.com>
2023-08-04 14:28:54 +0900

    feat180: 자유게시판 api 연결 S09P12B106-340

--- frontend/src/components/FreeBoard/PostDetail/PostDetail.tsx
+++ frontend/src/components/FreeBoard/PostDetail/PostDetail.tsx
@@ -3,43 +3,61 @@ import { useParams, useNavigate } from "react-router-dom";
 import { BackGroundImg } from "../../BackGroundImg";
 import Header from "../../Header/Header";
 import Messenger from "../../Message/Messenger";
-// recoil
-import { useRecoilState } from "recoil";
-import { PostListState, PostListNum } from "../../../recoil/Community";
-// style
-  import {
-  PostDetailDesign
-} from "./PostDetail.style";
-
+import { getPost, deletePost } from "/src/api/FreeBoard";
+import { 
+  PostDetailDesign, 
+  DetailTitleDesign,
+  DetailInfoBoxDesign,
+  DetailInfoDesign,
+  DetailUserDesign,
+  DetailContentDesign
+ } from "./PostDetail.style";
+// import { PostDetailType } from "/src/type/Auth";
 
 
 function PostDetail() {
   const navigate = useNavigate() 
-  const goFreeBoard = () => {navigate("/freeboard")}
-  const UpdatePost = () => {navigate(`/freeboard/update/${id}`)}
-
-  const [posts, setPosts] = useRecoilState(PostListState);
-  const [num, setNum] = useRecoilState(PostListNum);
   const id = parseInt(useParams().id || "");
-  const detail = posts.filter(post => post.id == id);
-  const DeletePost = () => {
-    const newposts = posts.filter(post => post.id !== id);
-    setPosts(newposts);
-    goFreeBoard();
-  }
+  const [post, setPost] = useState({});
+  const UpdatePost = () => {navigate(`/freeboard/update/${id}`)};
+  const DeletePost = () => {deletePost(id), navigate("/freeboard")}
+
+  useEffect(() => {
+    getPost(id).then((dataPost) => {
+      if (dataPost) {
+        setPost(dataPost)
+      }
+    })
+  }, [id])
 
   return (
     <BackGroundImg>
     <Header/>
     <PostDetailDesign>
-    <br/>
-    <h3>{detail[0].title}</h3>
-    <hr/>
-    <div>{detail[0].content}</div>
-    <br/>
-    <div><button onClick={() => UpdatePost()}>수정하기</button></div>
-    <br/>
-    <div><button onClick={() => DeletePost()}>삭제하기</button></div>
+
+      <DetailTitleDesign>
+        <h3>{post.title}</h3>
+      </DetailTitleDesign>
+
+      <DetailInfoBoxDesign>
+        <DetailInfoDesign>
+          <img style={{height: '2.5vh', marginLeft: '1vw'}} src="/src/assets/PostHit.png" alt="" /><span style={{marginLeft: '0.5vw'}}/>{post.hits}
+          <img style={{height: '2.5vh', marginLeft: '1vw'}} src="/src/assets/PostComment.png" alt="" /><span style={{marginLeft: '0.5vw'}}/>3
+          <img style={{height: '2.5vh', marginLeft: '1vw'}} src="/src/assets/PostLike.png" alt="" /><span style={{marginLeft: '0.5vw'}}/>{post.likes}
+        </DetailInfoDesign>
+        <DetailUserDesign>
+          <img style={{marginRight: '1vw', height: '5vh'}} src="/src/assets/Header/profile_tmp.png" alt="profileImg"/>
+          {post.nickname}
+          {` | ${post.createdAt?.slice(0, 10)} ${post.createdAt?.slice(11, 16)}`}
+        </DetailUserDesign>
+      </DetailInfoBoxDesign>
+     
+      <DetailContentDesign>
+        <div>{post.content}</div>
+      </DetailContentDesign>
+
+      <button onClick={() => UpdatePost()}>수정하기</button>
+      <button onClick={() => DeletePost()}>삭제하기</button>
     </PostDetailDesign>
     <Messenger/>
   </BackGroundImg>
