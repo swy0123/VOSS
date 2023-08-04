3098d0a2fa6287c25e86b92264976a0134818017
정현우 <gusehd502@naver.com>
2023-08-04 14:34:49 +0900

    fix: 배포준비완료 S09P12B106-358

--- frontend/src/components/Profile/BadgeBox/BadgeBox.tsx
+++ frontend/src/components/Profile/BadgeBox/BadgeBox.tsx
@@ -14,7 +14,10 @@ function BadgeBox() {
   const [profile, setProfile] = useRecoilState(ProfileState)
   const [tmp, setTmp] = useState(["더미","더미","더미","더미","더미","더미","더미","더미","더미"])
   return (
-    <BadgeBoxDesign>
+    <BadgeBoxDesign
+      style={{
+      paddingBottom: "70px"
+    }}>
       {/* 임시 방편 수정 해주세요!! */}
       <BadgeTitleDesign>활동 뱃지</BadgeTitleDesign>      
       {/* <BadgeContentDesign><img src={BadgeTemp} alt=""/></BadgeContentDesign>
@@ -22,11 +25,16 @@ function BadgeBox() {
         <span>{badge}</span>
       ))} */}
 
-      <BadgeContentDesign>
+      <BadgeContentDesign
+        style={{
+          marginLeft:"130px",
+          paddingBottom: "100px"
+        }}>
         {tmp.map((badge,index) => (
           <img style={{
             height : "200px",
-            width:"200px"}} src={`/src/assets/Profile/badge/B${index+1}.png`} alt=""/>
+            width:"200px",
+            margin:"30px"}} src={`/src/assets/Profile/badge/B${index+1}.png`} alt=""/>
         ))}
       </BadgeContentDesign>
 
