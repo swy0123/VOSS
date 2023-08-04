a9a7758ba9ff3108e1d2fce0f6f97f600724f756
이승종 <maeugel_rosh@naver.com>
2023-08-04 17:53:47 +0900

    feat: STOMP Config S09P12B106-336

---
+++ backend/src/main/java/com/yukgaejang/voss/global/config/StompWebSocketConfig.java
@@ -0,0 +1,39 @@
+package com.yukgaejang.voss.global.config;
+
+import com.yukgaejang.voss.domain.messenger.websocket.StompHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompHandler stompHandler;
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //해당 파라미터의 접두사가 붙은 목적지(구독자)에 메시지를 보낼
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //엔드포인트 추가 등록
+        registry
+                .addEndpoint("/websocket-endpoint")
+                .addInterceptors()
+                .setAllowedOriginPatterns("*");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
+    }
+}
