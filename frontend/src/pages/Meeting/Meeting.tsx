826ff2016fbf0ae1b8b78be19122400eb20a3e18
이시영 <swy05123@gmail.com>
2023-08-04 13:46:33 +0900

    feat:화상회의방 수정S09P12B106-291

--- frontend/src/pages/Meeting/Meeting.tsx
+++ frontend/src/pages/Meeting/Meeting.tsx
@@ -2,6 +2,7 @@ import { BackGroundImg } from "../../components/BackGroundImg";
 import { styled } from "styled-components";
 import Messenger from "../../components/Message/Messenger";
 import React, { ChangeEvent, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router";
 import { BottomSection, Container } from "./Meeting.style";
 import MeetJoin from "../../components/Meeting/OpenVidu/MeetJoin";
@@ -11,7 +12,22 @@ import { MeetingProps } from "../../api/meeting";
 
 function Meeting() {
   const { state } = useLocation(); // 2번 라인
+  useEffect(() => {
+    (() => {
+      window.addEventListener("beforeunload", onbeforeunload);
+    })();
+
+    return () => {
+      window.removeEventListener("beforeunload", onbeforeunload);
+    };
+  }, []);
+
+  const onbeforeunload = (event: BeforeUnloadEvent) => {
+    event.preventDefault();
+    event.returnValue = ''; 
+  };
   const [bottomOn, setBottomOn] = useState(false);
+
 
   const isBottomOn = () =>{
     setBottomOn(!bottomOn);
