1a96a48bb5a21f82a28f5706f5e65268c39e9219
이원영 <sheltonwon@gmail.com>
2023-08-06 01:14:58 +0900

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,36 @@
+// 로컬 환경
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     // proxy: {
+//     //   '/api': {
+//     //     target: 'http://i9b106.p.ssafy.io:8080',
+//     //     changeOrigin: true,
+//     //   },
+//     // },
+//   },
+//   css:{
+//     devSourcemap: true
+//   }
+// })
+
+
+// 배포 환경
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://i9b106.p.ssafy.io:8080',
-    //     changeOrigin: true,
-    //   },
-    // },
+    host: true,
+    port: 5173,
+    https: {
+       key: 'privkey.pem',
+       cert: 'fullchain.pem',
+     },
   },
-  css:{
-    devSourcemap: true
-  }
+  plugins: [ react() ],
 })
