90be04c61baeb442d6416c1d794b6280666199e3
정현우 <gusehd502@naver.com>
2023-08-08 03:24:06 +0900

    init : 스크립트 스크롤 구현 시작 S09P12B106-456

--- frontend/src/components/DubbingRoom/Script/Script.tsx
+++ frontend/src/components/DubbingRoom/Script/Script.tsx
@@ -1,6 +1,7 @@
 import { Line } from '../../../type/type';
 import { useState, useEffect, useRef } from 'react'
 import { useRecoilState } from 'recoil';
+import { Link, animateScroll as scroll } from 'react-scroll';
 import { 
   PlayChangebState, 
   ScriptSelectState } from '../../../recoil/Training';
@@ -22,14 +23,15 @@ function Script ({lines}: VideoProps) {
   const [playChange, setPlayChange] = useRecoilState<number[]>(PlayChangebState)
   const [time, setTime] = useState(0);
   const intervalRef = useRef<number|null>(null);
+  const ScrollRef = useRef<number>(0)
 
   const fixTopScript = () => {
     // 1은 PlayingState
     if(playChange[0] === 1) { 
-      setTime(playChange[1] * 1000);
+      setTime(playChange[1] * 1);
       intervalRef.current = setInterval(() => {
-        setTime((prevTime) => prevTime + 10);
-      }, 10);
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
     }
 
     // 2은 PausedState
@@ -38,16 +40,30 @@ function Script ({lines}: VideoProps) {
     }
   }
 
+  useEffect(() => {
+
+    const position = document.getElementById(`${time}`)?.getBoundingClientRect()
+    console.log(position)
+
+    
+    // if (position) {
+    //   ScrollRef.current.scrollTop = position;
+    // }
+    console.log("너니?",time);
+  }, [time]);
+
   useEffect(()=> {
     fixTopScript()
   },[playChange])
 
   return(
     <Container>
-      {/* <div style={{color:'white'}}>{time}</div> */}
-      <ScriptBox>
+      <div style={{color:'white'}}>{time}</div>
+      <ScriptBox 
+        ref={ScrollRef}>
         {lines.map((line,index) => (
           <Scripts
+            id={line.startSec}
             key={index}
             $IsClick={isScriptSelect[index]}>
             <Role>{line.name}</Role>
