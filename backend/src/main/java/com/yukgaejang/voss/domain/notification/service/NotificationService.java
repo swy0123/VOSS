87610c0ceeb851c83fb8fee603e050543fa18b20
210-reverof <sheltonwon@gmail.com>
2023-08-07 22:58:30 +0900

    feat: 댓글, 좋아요 알림 구현 S09P12B106-429

--- backend/src/main/java/com/yukgaejang/voss/domain/notification/service/NotificationService.java
+++ backend/src/main/java/com/yukgaejang/voss/domain/notification/service/NotificationService.java
@@ -1,5 +1,7 @@
 package com.yukgaejang.voss.domain.notification.service;
 
+import com.yukgaejang.voss.domain.freeboard.repository.entity.PostComment;
+import com.yukgaejang.voss.domain.freeboard.repository.entity.PostLike;
 import com.yukgaejang.voss.domain.member.repository.entity.Follow;
 import com.yukgaejang.voss.domain.notification.repository.NotificationRepository;
 import com.yukgaejang.voss.domain.notification.repository.entity.Notification;
@@ -9,5 +11,7 @@ import org.springframework.stereotype.Service;
 
 
 public interface NotificationService {
-    public void notifyFollow(Follow follow);
+    void notifyFollow(Follow follow);
+    void notifyPostComment(PostComment postComment);
+    void notifyPostLike(PostLike postLike);
 }
