450c6d3e8ef3837f345b4f903e5eb16067bf9de1
김준섭 <vacanter@naver.com>
2023-08-09 15:10:08 +0900

    feat145: 메신저 실시간 채팅 S09P12B106-393

--- frontend/src/components/Message/MessageRoom/MessageRoom.tsx
+++ frontend/src/components/Message/MessageRoom/MessageRoom.tsx
@@ -22,122 +22,163 @@ import {
   Input, 
   Send, } from "./MessageRoom.style"
 
+const WebSocket_URL = 'wss:/i9b106.p.ssafy.io:8080/ws/messenger'
+
 const MessageRoom = () => {
-  const [currentUser, setCurrentUser] = useRecoilState(CurrentUserAtom);
+  const [currentUser, setCurrentUser] = useRecoilState(CurrentUserAtom);  
   const [isOpenRoom, setOpenRoom] = useRecoilState<boolean>(ShowMessageRoomState);
   const [currentRoom, setCurrentRoom] = useRecoilState<CurrentRoomType>(CurrentRoomState);
   const [messages, setMessages] = useRecoilState<MessageType[]>(MessagesState);
   const [message, setMessage] = useState<string>("");
-  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const socketRef = useRef<WebSocket | null>(null);
   const [exitBtnHover, setExitBtnHover] = useState(false);
   const [sendHover, setSendHover] = useState(false);
-  const [currentPage, setCurrentPage] = useState(0);
-  const [totalPages, setTotlaPages] = useState(0);
-
-  useEffect(() => {
-  // 웹소캣 연결을 수행합니다.
-  const ws = new WebSocket(`wss:/i9b106.p.ssafy.io:8080/ws/messenger`);
-
-  ws.onopen = () => { 
-    setSocket(ws);
-  }
-    // console.log("websocket open")
-
-    // 서버로부터 메시지를 수신할 때의 처리를 등록합니다.
-  ws.onmessage = (event) => {
-    
-    console.log("event.data: ", event.data);
-    const newMessage = JSON.parse(event.data)
-    if (newMessage.memberId !== currentUser.userid) {
-      setMessages((prevMessages) => [...prevMessages, newMessage]);
-    }
-  };
-
-  // return () => {
-  //   if (socket) {
-  //     socket.close();
-  //     // console.log("websocket close")
-  //   }
-  // };
-  }, []);
 
   const sendMessage = () => {
     if (message.trim() !== "") {
-
       const sentMessage = {
         chatId: currentRoom.chatId,
         sessionId: currentRoom.sessionId,
         memberId: currentUser.userid,
         content: message,
       };
-
-  // const sentAlarm = {
-  //   chatId: 1,
-  //   sessionId: "init",
-  //   memberId: currentRoom.memberId,
-  //   content: "Alarm",
-  // };
-  
-  socket.send(JSON.stringify(sentMessage));
-  setMessages((prevMessages) => [...prevMessages, sentMessage]);
-  // socket.send(JSON.stringify(sentAlarm));
-  setMessage("");
-}
+      const sentAlarm = {
+        chatId: 1,
+        sessionId: "init",
+        memberId: currentRoom.memberId,
+        content: "Alarm",
+      };
+      if (socketRef.current) {
+        socketRef.current.send(JSON.stringify(sentMessage));
+        socketRef.current.send(JSON.stringify(sentAlarm));
+        setMessage("");
+      }
+    }
   };
+
+  const sendEnterMessage = () => {
+    const enterMessage = {
+      chatId: currentRoom.chatId,
+      sessionId: currentRoom.sessionId,
+      memberId: currentUser.userid,
+      content: "enter",
+    };
+    if (socketRef.current) {
+    socketRef.current.send(JSON.stringify(enterMessage));
+    };
+  };
+  
+  const sendLeaveMessage = () => {
+    const leaveMessage = {
+        chatId: currentRoom.chatId,
+        sessionId: currentRoom.sessionId,
+        memberId: currentUser.userid,
+        content: "leave",
+      };
+    if (socketRef.current) {
+    socketRef.current.send(JSON.stringify(leaveMessage));
+    socketRef.current.close();
+    };
+  };
+
+  useEffect(() => {
+    console.log("messageRoom mounted")
+    
+    if (!socketRef.current) {
+      const ws = new WebSocket(WebSocket_URL);
+      // const ws = new WebSocket(`wss:/localhost:8080/ws/messenger`);
+      
+      ws.onopen = () => { 
+        socketRef.current = ws; 
+        console.log("websocket open")
+        sendEnterMessage();
+      };
+    
+      ws.onmessage = (event) => {
+        let recieveMessage = JSON.parse(event.data);
+        console.log("memberId: ", recieveMessage.memberId, "userId: ", currentUser.userid, "recieveMessage: ", recieveMessage);
+
+        if (recieveMessage.sessionId != "init") {
+          setMessages((prevMessages) => [...prevMessages, JSON.parse(event.data)]);
+        }
+      };
+
+      ws.onclose = () => {
+        console.log("websocket close")
+        if (isOpenRoom) {
+          const ws = new WebSocket(WebSocket_URL);
+          ws.onopen = () => { 
+            socketRef.current = ws; 
+            console.log("websocket open")
+            sendEnterMessage();
+          };
+        };
+      };
+    };
+
+    return () => {
+      if (socketRef.current) {
+        sendLeaveMessage();
+        console.log("websocket close if");
+      } else {
+        console.log("websocket close else");
+      }
+    };
+
+  }, []);
 
   return (
     <MessegeListDiv>
 
-  <div>
-    <MessegeTitle>{currentRoom.name}</MessegeTitle>
-    <ExitImg
-        src={exitBtnHover ? ExitBoxHover : ExitBox}
-        onClickCapture={()=>setOpenRoom(false)}
-        onMouseEnter={() => setExitBtnHover(true)}
-        onMouseLeave={() => setExitBtnHover(false)}
-    /><hr />
-  </div>
+      <div>
+        <MessegeTitle>{currentRoom.name}</MessegeTitle>
+        <ExitImg
+            src={exitBtnHover ? ExitBoxHover : ExitBox}
+            onClickCapture={()=>setOpenRoom(false)}
+            onMouseEnter={() => setExitBtnHover(true)}
+            onMouseLeave={() => setExitBtnHover(false)}
+        /><hr />
+      </div>
 
-  <InfinityScroll style={{ position: "relative" }}>
-    {messages.map((message: MessageType, index: number) =>
-      message.memberId === currentUser.userid
-      ? (
-        <div key={index}>
-          <MyChatting>
-            <ChattingDate>{message.time}</ChattingDate>
-            <Chatting> {message.content}</Chatting>
-          </MyChatting>
+      <InfinityScroll style={{ position: "relative" }}>
+        {messages.map((message: MessageType, index: number) =>
+          message.memberId === currentUser.userid
+          ? (
+            <div key={index}>
+              <MyChatting>
+                <ChattingDate>{message.time?.slice(0, 10)} {message.time?.slice(11, 17)}</ChattingDate>
+                <Chatting> {message.content}</Chatting>
+              </MyChatting>
+            </div>
+          ) : (
+            <div key={index}>
+              <OtherChatting>
+                <Chatting> {message.content}</Chatting>
+                <ChattingDate>{message.time?.slice(0, 10)} {message.time?.slice(11, 17)}</ChattingDate>
+              </OtherChatting>
+            </div>
+          )
+        )}
+      </InfinityScroll>
+
+      <MessegeBodyDiv>
+        <div>
+          <Input 
+          className="input"
+          type="text" 
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setMessage(e.target.value)} 
+          value={message}
+          />
+          <Send
+          style={{ width: '30px', height: '30px' }}
+          src={ sendHover ? ArrowHover :Arrow}
+          onMouseEnter={() => setSendHover(true)}
+          onMouseLeave={() => setSendHover(false)}
+          onClick={sendMessage}/>
         </div>
-      ) : (
-        <div key={index}>
-          <OtherChatting>
-            <Chatting> {message.content}</Chatting>
-            <ChattingDate>{message.time}</ChattingDate>
-          </OtherChatting>
-        </div>
-      )
-    )}
+      </MessegeBodyDiv>
 
-    {/* <div ref={startRef}></div> */}
-  </InfinityScroll>
-
-  <MessegeBodyDiv>
-    <div>
-      <Input 
-      className="input"
-      type="text" 
-      onChange={(e: ChangeEvent<HTMLInputElement>) => setMessage(e.target.value)} 
-      value={message}/>
-      <Send
-      style={{ width: '30px', height: '30px' }}
-      src={ sendHover ? ArrowHover :Arrow}
-      onMouseEnter={() => setSendHover(true)}
-      onMouseLeave={() => setSendHover(false)}
-      onClick={sendMessage}/>
-    </div>
-  </MessegeBodyDiv>
-
-</MessegeListDiv>
+    </MessegeListDiv>
   );
 };
 
