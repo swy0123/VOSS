538b8cfa3a3156808d379c42146ebac926198086
정현우 <gusehd502@naver.com>
2023-08-10 13:38:10 +0900

    style: 더빙리스트 스타일 수정 및 스크롤 커스텀 S09P12B106-451

--- frontend/src/components/DubbingList/Title/Title.style.ts
+++ frontend/src/components/DubbingList/Title/Title.style.ts
@@ -4,7 +4,7 @@ export const TitleBox = styled.div`
   display: flex;
   color: white;
   align-items: flex-end;
-  margin: 10px 0px 13px 0px;
+  margin: 2.5% 0px 13px 0px;
 `
 export const MainTitle = styled.h2`
   margin: 0px 20px 0px 0px;
