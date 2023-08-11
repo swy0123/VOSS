a7a09dffbd65a89ac354eb774382fbf823da0ddf
김준섭 <vacanter@naver.com>
2023-08-11 10:47:42 +0900

    feat257: 마이페이지 도넛차트 배지모달

--- frontend/src/components/Profile/BadgeBox/ProfileBadgeModal.tsx
+++ frontend/src/components/Profile/BadgeBox/ProfileBadgeModal.tsx
@@ -1,6 +1,7 @@
 import React, { PropsWithChildren, useState, MouseEvent, useEffect } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { BadgeModalShowState, BadgesState, ProfileState } from "/src/recoil/Auth";
+import { BadgeType } from "/src/type/Auth";
 import { selectedMember } from "/src/recoil/Meeting";
 import {
   ModalContainer,
@@ -10,48 +11,61 @@ import {
   BadgeContent,
   MeetBadgeDiv,
   MeetBadgeImg,
+  MeetBadgeImg2,
   MeetBadgeHovor,
 } from "./ProfileBadgeModal.style";
 
+interface BadgeListType {
+  id: number,
+  name: string,
+};  
 
 const BadgeModal = () => {
   const [hover, setHover] = useState<number>(0);
-  const badges = useRecoilValue(ProfileState).badges
-  const badgeList = useRecoilValue(BadgesState);
+  const [hover2, setHover2] = useState<number>(0);
+  const badges: BadgeType[] = useRecoilValue(ProfileState).badges
+  const badgeList = useRecoilValue<BadgeListType[]>(BadgesState);
   const [showModal, setShowModal] = useRecoilState(BadgeModalShowState);
 
   return (
     <ModalContainer>
       <DialogBox>
         <ModalHeader>
-          {/* <h3>마우스를 올리면 배지의 설명이 나타납니다.</h3> */}
+          <h3>마우스를 올리면 배지의 설명이 나타납니다.</h3>
         </ModalHeader>
-
-        <h4 style={{color: "white"}} >내가 보유한 뱃지</h4>
+        <br/>
 
         <BadgeContent>
           {badges !== undefined ? (
             badges.map((badge) => (
               <MeetBadgeDiv key={badge.id}>
                 <MeetBadgeImg
-                  onMouseEnter={() => setHover(badge.id)}
+                  onMouseEnter={() => setHover(badge.id||0)}
                   onMouseLeave={() => setHover(0)}
-                  src={`/src/assets/Profile/badge/B${badge.id + 1}.png`}
+                  src={`/src/assets/Profile/badge/B${badge.id}.png`}
                   alt=""
                 />
-                {hover !== 0 && hover === badge.id ? (
+                {/* { !hover
+                ? 
+                && hover === badge.id ? ( */}
                   <MeetBadgeHovor $hoverActive={hover}>
-                    <div className="hover-text"> {badge.name}</div>
+                    { hover === 0
+                    ? badge.cnt
+                    : hover === badge.id
+                      ? badge.name
+                      : "\u00A0"
+                    }
+                    {/* <div className="hover-text"> {badge.name}</div> */}
                   </MeetBadgeHovor>
-                ) : (
-                  <></>
-                )}
+                {/* ) : (
+                  <MeetBadgeHovor>{badge.cnt}</MeetBadgeHovor>
+                )} */}
               </MeetBadgeDiv>
             ))
           ) : null }
         </BadgeContent>
         
-        <hr style={{width: '400px'}}></hr>
+        <hr style={{width: '400px', opacity: '0.5'}}></hr>
 
         <BadgeContent>
           {badgeList !== undefined ? (
@@ -59,14 +73,14 @@ const BadgeModal = () => {
               (!badges.some(item => item.id === badge.id)) ?
 
                 <MeetBadgeDiv key={badge.id}>
-                  <MeetBadgeImg
-                    onMouseEnter={() => setHover(badge.id)}
-                    onMouseLeave={() => setHover(0)}
-                    src={`/src/assets/Profile/badge/B${badge.id + 1}.png`}
+                  <MeetBadgeImg2
+                    onMouseEnter={() => setHover2(badge.id)}
+                    onMouseLeave={() => setHover2(0)}
+                    src={`/src/assets/Profile/badge/B${badge.id}.png`}
                     alt=""
                   />
-                  {hover !== 0 && hover === badge.id ? (
-                    <MeetBadgeHovor $hoverActive={hover}>
+                  {hover2 !== 0 && hover2 === badge.id ? (
+                    <MeetBadgeHovor $hoverActive={hover2}>
                       <div className="hover-text"> {badge.name}</div>
                     </MeetBadgeHovor>
                   ) : (
