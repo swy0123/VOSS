be8c391bd351682a5f6f2995f824ba0e2603f10c
이승종 <maeugel_rosh@naver.com>
2023-08-12 21:21:18 +0900

    fix: 오류 해결

--- frontend/src/pages/SelectCategory/SelectCategory.tsx
+++ frontend/src/pages/SelectCategory/SelectCategory.tsx
@@ -1,138 +1,140 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { postRractice } from '/src/api/profile';
-import { 
+import {
   Accent,
   AllBoard,
-  AllCategory, 
-  AllSinglePractice, 
-  Cartegory_units, 
-  Community, 
-  Description, 
-  Dubbing, 
-  Free, 
-  Icon, 
-  MainImg, 
-  Meeting, 
-  MeetingEnter, 
-  MeetingEnterIcon, 
-  Record, 
-  Title, 
+  AllCategory,
+  AllSinglePractice,
+  Cartegory_units,
+  Community,
+  Description,
+  Dubbing,
+  Free,
+  Icon,
+  MainImg,
+  Meeting,
+  MeetingEnter,
+  MeetingEnterIcon,
+  Record,
+  Title,
   Training,
   UnSkew,
-  VoiceAnalysis} from './SelectCategory.style';
+  VoiceAnalysis,
+} from './SelectCategory.style';
 import Header from '/src/components/SelectCategotyHeader/Header';
 
-function SelectCategory () {
-  const [TrainIsShown, setTrainIsShown] = useState(false)
-  const [MeetIsShown, setMeetIsShown] = useState(false)
-  const [CommunityIsShown, setCommunityIsShown] = useState(false)
-  const [, setMicrophonePermission] = useState<boolean | null>(null)
-  const [, setCameraPermission] = useState<boolean | null>(null)
+function SelectCategory() {
+  const [TrainIsShown, setTrainIsShown] = useState(false);
+  const [MeetIsShown, setMeetIsShown] = useState(false);
+  const [CommunityIsShown, setCommunityIsShown] = useState(false);
 
   // Router Link와 동일한 부분
-  const navigate = useNavigate() 
-
-  useEffect(() => {
-    async function requestMediaPermissions() {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          audio: true,
-          video: true,
-        });
-
-        setMicrophonePermission(true);
-        setCameraPermission(true);
-
-        stream.getTracks().forEach((track) => track.stop());
-      } catch (error) {
-        setMicrophonePermission(false);
-        setCameraPermission(false);
-      }
-    }
-
-    requestMediaPermissions();
-  }, []);
+  const navigate = useNavigate();
 
   const goVoiceAnalysis = () => {
-    void postRractice("ACT")
-    navigate("/analysis")
-  }
+    void postRractice('ACT');
+    navigate('/analysis');
+  };
   const goDubbingList = () => {
-    navigate("/dubbinglist")
-    window.location.reload()
-  }
+    navigate('/dubbinglist');
+    window.location.reload();
+  };
   const goAccent = () => {
-    void postRractice("DICTION")
-    navigate("/accent")
-  }
+    void postRractice('DICTION');
+    navigate('/accent');
+  };
   const goFreeBoard = () => {
-    navigate("/freeboard")
-  }
+    navigate('/freeboard');
+  };
   const goMeetingBoard = () => {
-    navigate("/meeting")
-  }
+    navigate('/meeting');
+  };
   const goGame = () => {
-    navigate("/game")
-  }
+    navigate('/game');
+  };
 
-  return(
+  return (
     <div>
       <MainImg>
-      <Header/>
+        <Header />
         <AllCategory>
-          
-          <Training 
+          <Training
             onMouseEnter={() => setTrainIsShown(true)}
             onMouseLeave={() => setTrainIsShown(false)}
-            >
-            <Cartegory_units>  
-              <Icon src="/src/assets/Category/Training.png"/>
+          >
+            <Cartegory_units>
+              <Icon src="/src/assets/Category/Training.png" />
               <Title>Training</Title>
-              <Description>더빙 연습, 목소리 나이대 및 성별 분석, 발음교정 연습이 가능한 Private 연습공간</Description>
+              <Description>
+                더빙 연습, 목소리 나이대 및 성별 분석, 발음교정 연습이 가능한
+                Private 연습공간
+              </Description>
             </Cartegory_units>
-            
+
             <AllSinglePractice $isShown={TrainIsShown}>
-              <VoiceAnalysis onClick={goVoiceAnalysis}><UnSkew>목소리 분석</UnSkew></VoiceAnalysis>
-              <Dubbing onClick={goDubbingList}><UnSkew>더빙 연습</UnSkew></Dubbing>
-              <Accent onClick={goAccent}><UnSkew>발음 연습</UnSkew></Accent>
+              <VoiceAnalysis onClick={goVoiceAnalysis}>
+                <UnSkew>목소리 분석</UnSkew>
+              </VoiceAnalysis>
+              <Dubbing onClick={goDubbingList}>
+                <UnSkew>더빙 연습</UnSkew>
+              </Dubbing>
+              <Accent onClick={goAccent}>
+                <UnSkew>발음 연습</UnSkew>
+              </Accent>
             </AllSinglePractice>
           </Training>
-        
+
           <Meeting
             onMouseEnter={() => setMeetIsShown(true)}
-            onMouseLeave={() => setMeetIsShown(false)}>
+            onMouseLeave={() => setMeetIsShown(false)}
+          >
             <Cartegory_units>
-              <Icon src="/src/assets/Category/Meeting.png"/>
+              <Icon src="/src/assets/Category/Meeting.png" />
               <Title>Meeting</Title>
-              <Description>다른 유저들과 함께 연습 할 수 있는 Meeting Room</Description>
+              <Description>
+                다른 유저들과 함께 연습 할 수 있는 Meeting Room
+              </Description>
             </Cartegory_units>
-            
+
             <MeetingEnter $isShown={MeetIsShown}>
-            <UnSkew><MeetingEnterIcon src="/src/assets/Category/EnterMeeting.png" onClick={goMeetingBoard}/></UnSkew>
+              <UnSkew>
+                <MeetingEnterIcon
+                  src="/src/assets/Category/EnterMeeting.png"
+                  onClick={goMeetingBoard}
+                />
+              </UnSkew>
             </MeetingEnter>
           </Meeting>
 
           <Community
             onMouseEnter={() => setCommunityIsShown(true)}
-            onMouseLeave={() => setCommunityIsShown(false)}>
+            onMouseLeave={() => setCommunityIsShown(false)}
+          >
             <Cartegory_units>
-              <Icon src="/src/assets/Category/Community.png"/>
+              <Icon src="/src/assets/Category/Community.png" />
               <Title>Community</Title>
-              <Description>나만의 연습 방법을 공유하고, 연습 기록을 공유하는 게시판</Description>
+              <Description>
+                나만의 연습 방법을 공유하고, 연습 기록을 공유하는 게시판
+              </Description>
             </Cartegory_units>
 
             <AllBoard $isShown={CommunityIsShown}>
-              <Free onClick={goFreeBoard}><UnSkew>자유 게시판</UnSkew></Free>
-              <Record><UnSkew>녹음 게시판</UnSkew></Record>
-              <Free onClick={goGame}><UnSkew>목소리 마피아</UnSkew></Free>
+              <Free onClick={goFreeBoard}>
+                <UnSkew>자유 게시판</UnSkew>
+              </Free>
+              <Record>
+                <UnSkew>녹음 게시판</UnSkew>
+              </Record>
+              <Free onClick={goGame}>
+                <UnSkew>목소리 마피아</UnSkew>
+              </Free>
             </AllBoard>
           </Community>
-
         </AllCategory>
       </MainImg>
     </div>
-  )
+  );
 }
 
-export default SelectCategory
+export default SelectCategory;
