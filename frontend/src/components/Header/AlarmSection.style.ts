d5c2d001a9fe1772d301731271e74156b2bf43a9
정현우 <gusehd502@naver.com>
2023-08-12 22:33:51 +0900

    feat: 알림 데이터 전체 가져오기 S09P12B106-443

--- frontend/src/components/Header/AlarmSection.style.ts
+++ frontend/src/components/Header/AlarmSection.style.ts
@@ -47,3 +47,25 @@ export const AlarmExitBtn = styled.img`
   margin-right: 5px;
   cursor: pointer;
 `
+
+export const AlarmBox = styled.div`
+`
+
+export const AlarmList = styled.div`
+  overflow-y: scroll;
+`
+
+export const AlarmItem = styled.div`
+  background-color: #efefef;
+  border-radius: 4px;
+  margin: 8px 10px 0px 10px;
+  padding: 10px;
+  font-size: 11px;
+`
+
+export const AlarmTime = styled.div`
+  font-size: 1px;
+  margin-right: 10px;
+  text-align: right;
+  color: #797979;
+`
