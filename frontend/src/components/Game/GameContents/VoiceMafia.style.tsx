f71833ef885cd68bba93d8be4330bfbf8380f369
suyeonsu <agew0423@daum.net>
2023-08-14 03:37:04 +0900

    feat: 게임 진행 페이지

--- frontend/src/components/Game/GameContents/VoiceMafia.style.tsx
+++ frontend/src/components/Game/GameContents/VoiceMafia.style.tsx
@@ -36,3 +36,58 @@ export const StartButton = styled.div`
     transition: 0.2s;
   }
 `;
+
+export const PlayExplain = styled.div`
+  margin-top: 80px;
+  font-size: 25px;
+  color: white;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledDivWithText = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.3);
+  border-radius: 8px;
+  color: white;
+  padding: 20px;
+  width: 60%;
+  margin: 60px auto;
+`;
+
+// display: flex;
+// flex-direction: column;
+// justify-content: flex-start;
+
+// padding: 6px 0px 6px 0px;
+// height: 210px;
+
+export const OptionButtonContainer = styled.div`
+  display: flex; /* 가로 정렬을 위해 플렉스 컨테이너로 설정 */
+  flex-direction: row; /* 가로 방향으로 정렬 */
+  flex-wrap: wrap; /* 컨테이너의 크기를 넘어갈 경우 줄바꿈 */
+  justify-content: center; /* 가로 방향 가운데 정렬 */
+`;
+
+export const OptionButton = styled.div<{ $IsColor: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 15px;
+  background-color: ${(props) => (props.$IsColor ? "#ffc000" : "white")};
+  width: 170px;
+  height: 120px;
+  margin: 8px;
+  font-size: 16px;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: 0 0 20px ${(props) => (props.$IsColor ? "#ffc000" : "white")};
+    transition: 0.2s;
+  }
+`;
+
