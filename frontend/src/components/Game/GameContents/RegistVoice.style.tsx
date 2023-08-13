0672208483a41483207fe9cfa3e31841bc7b42ec
이승종 <maeugel_rosh@naver.com>
2023-08-14 00:02:26 +0900

    feat: 보이스 등록 시작페이지

--- frontend/src/components/Game/GameContents/RegistVoice.style.tsx
+++ frontend/src/components/Game/GameContents/RegistVoice.style.tsx
@@ -1,6 +1,39 @@
-import { styled } from "styled-components";
+import { styled } from 'styled-components';
 
 export const GameNoticeDiv = styled.div`
   width: 90%;
   height: 90%;
+`;
+
+export const GameTitle = styled.img`
+  width: 45%;
+  height: 9%;
+`;
+export const GameExplain = styled.div`
+  margin-top: 30px;
+  font-size: 25px;
+  color: white;
+`;
+
+export const RecordButton = styled.div`
+  position: absolute;
+  bottom: 10%;
+  left: 50%;
+  transform: translate(-50%, 0);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 15px;
+  background-color: #107e5d;
+  width: 170px;
+  height: 31px;
+  margin: 8px;
+  font-size: 16px;
+  color: white;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: 0 0 20px #107e5d;
+    transition: 0.2s;
+  }
 `;
--- frontend/src/components/Game/GameContents/RegistVoice.tsx
+++ frontend/src/components/Game/GameContents/RegistVoice.tsx
@@ -1,17 +1,27 @@
-import { GameMainContainer } from "../GameMain/GameMain.style"
+import { GameMainContainer } from '../GameMain/GameMain.style';
+
+import {
+  GameExplain,
+  GameNoticeDiv,
+  GameTitle,
+  RecordButton,
+} from './RegistVoice.style';
+import GameTitleImg from '/src/assets/Game/GameTitleImg.png';
 
 function RegistVoice() {
-
-    return(
-        <GameMainContainer>
-            RegistVoice
-
-        </GameMainContainer>
-
-    )
-
-
-
+  return (
+    <GameMainContainer>
+      <GameNoticeDiv>
+        <GameTitle src={GameTitleImg} />
+        <GameExplain>
+          내 목소리를 보이스 마피아에 등록해보세요!
+          <br />
+          등록한 목소리는 인게임에서 랜덤하게 만나볼 수 있습니다.
+        </GameExplain>
+        <RecordButton>녹음하기</RecordButton>
+      </GameNoticeDiv>
+    </GameMainContainer>
+  );
 }
 
-export default RegistVoice
+export default RegistVoice;
