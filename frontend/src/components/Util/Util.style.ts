38893609e5611f2abf59c312f85a97aad831de55
210-reverof <sheltonwon@gmail.com>
2023-08-14 21:15:03 +0900

    style: alert 디자인 수정 완료 S09P12B106-514

--- frontend/src/components/Util/Util.style.ts
+++ frontend/src/components/Util/Util.style.ts
@@ -25,11 +25,10 @@ export const DialogContainer = styled.div`
 
     width: 400px;
     padding: 20px;
-    border-radius: 8px;
     background-color: white;
-    /* background-color: rgba(0, 0, 0, 0.8); */
+    background-color: rgba(255, 255, 255, 0.95);
     /* backdrop-filter: blur(5px); */
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    box-shadow: 0 0 10px rgba(0, 0, 5, 0.3);
 
     .title {
       margin: 0 0 20px 0;
@@ -37,10 +36,10 @@ export const DialogContainer = styled.div`
 
     .input {
       width: 100%;
-      padding: 10px;
+      padding: 8px;
       border-radius: 8px;
-      border: 2px solid #ccc;
-      font-size: 1em;
+      border: 1px solid #ccc;
+      font-size: 14px;
       font-family: inherit;
       box-sizing: border-box;
       margin-top: 10px;
@@ -52,12 +51,14 @@ export const DialogContainer = styled.div`
     }
 
     .buttons button {
-      border: 1px solid #6c6c6c;  
-      background-color: white;
-      color: #6c6c6c;
+      /* border: 1px solid #6c6c6c;   */
+      border: none;
+      background-color: #132b31;
+      color: white;
       text-transform: uppercase;
       transition: all 0.2s;
-      font-weight: bold;
+      font-weight: medium;
+      border-radius: 4px;
       margin: 0 5px;
       width: 70px;
       height: 28px;
@@ -66,14 +67,15 @@ export const DialogContainer = styled.div`
 
     .buttons button:hover {
       
-      background-color: #6c6c6c;
+      background-color: #2a606e;
       color: white;
       border: 1px solid #6c6c6c;
     }
 
     .buttons button:focus {
-      border: 1px solid #6c6c6c;
-      border-radius: 4px;  
+      /* border: 1px solid #6c6c6c; */
+      border: none;
+      border-radius: 4px;
     }
   }
 `;
