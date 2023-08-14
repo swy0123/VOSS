b8a64ac9800344785c74434cf670667abc664cc2
210-reverof <sheltonwon@gmail.com>
2023-08-14 20:13:58 +0900

    fix: 프롬포트 수정 및 녹음파일 표시 형식 수정

--- frontend/src/components/Accent/Recording/Recording.tsx
+++ frontend/src/components/Accent/Recording/Recording.tsx
@@ -22,7 +22,7 @@ function Recording (){
     const hours = String(date.getHours()).padStart(2,"0")
     const minutes = String(date.getMinutes()).padStart(2,"0")
     const seconds = String(date.getSeconds()).padStart(2,"0")
-    const time = `${hours}-${minutes}-${seconds}`
+    const time = `${hours}:${minutes}`
     setTimeList([time,...timeList.slice(0,4)])
   }
 
@@ -36,12 +36,11 @@ function Recording (){
       <RecordBox>
         {accentRecord.map((file,index) => (
           <RecordItem key={index}>
-            <RecordSelect type="radio" name="record"/>
             <RecordLable>
-            "voss"{timeList[index]}.mp3
+            voss-{timeList[index]}.mp3
             </RecordLable>
             <audio src={file} controls style={{
-              width :'100px',
+              width :'200px',
               height : '28px',
             }}/>
             <a href={file} download="my-audio-file.mp3">
