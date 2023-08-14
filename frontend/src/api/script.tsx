79601c1cf6fd51be59f04adf9f20452c079e5062
이승종 <maeugel_rosh@naver.com>
2023-08-14 15:07:39 +0900

    fix: 카테고리 선택 에러 수정

--- frontend/src/api/script.tsx
+++ frontend/src/api/script.tsx
@@ -8,7 +8,7 @@ type AsyncFunction = (
 export const makeAnalysisScript: AsyncFunction = async(genderSelected,ageSelected) => {
   try{
     const response: AxiosResponse<string> = await privateApi.get<string>(
-      `/practice/diction/script?cmd=성별이${ageSelected}이고 ${genderSelected}인 성우지망생이 연습할 더빙 대사 60자 이내로 작성해줘`
+      `/practice/diction/script?cmd=성별이${ageSelected}이고 ${genderSelected}인 더빙 대사를 실제 드라마나 영화 애니메이션 등에 사용되었던 스크립트로 20자 이상 60자 이내로 작성해줘`
     )
     console.log(genderSelected)
     console.log(ageSelected)
@@ -22,8 +22,9 @@ export const makeAnalysisScript: AsyncFunction = async(genderSelected,ageSelecte
 
 export const makeAccentScript: AsyncFunction = async(categorySelected) => {
   try{
+    console.log(categorySelected+"    api")
     const response: AxiosResponse<string> = await privateApi.get<string>(
-      `/practice/diction/script?cmd=성우지망생이 연습할 ${categorySelected} 카테고리의 대사 60자 이내로 작성해줘`
+      `/practice/diction/script?cmd=${categorySelected} 의 해당하는 대사를 실제 영화나 드라마에 다왔던 대사 한 줄글로 내놔 어떤 스크립트인지는 알려주지 말고 ':' 표시 없고 특수문자 없이 40자 이상 60자 이내로`
     )
     return response.data
   }
