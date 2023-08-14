8017389d6e7f7c02f4a6f884a7afcfe99672efa7
정현우 <gusehd502@naver.com>
2023-08-14 16:56:25 +0900

    feat: 화상 더빙 동시 녹음 구현중

--- frontend/src/components/DubbingRoom/RecordButton/RecordButton.tsx
+++ frontend/src/components/DubbingRoom/RecordButton/RecordButton.tsx
@@ -131,14 +131,14 @@ function RecordButton({ script, lines }: VideoProps) {
     setRecordBtnClickable(true)
   }
 
-  // 연습이 멈춤 -> 재시작
+  // 연습 멈춤 -> 재시작
   const changePracticeEnd = () => {
     setPracticeStart(false)
     setPracticeEnd(true)
     setPlayTrigger(1)
   }
 
-  // 연습이 진행중 -> 정지
+  // 연습 진행중 -> 정지
   const changePracticeStart = () => {
     setPracticeStart(true)
     setPracticeEnd(false)
