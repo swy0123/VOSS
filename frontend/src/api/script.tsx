2c2550c81217b66260c5ce71a0f85da08e325a9d
210-reverof <sheltonwon@gmail.com>
2023-08-14 20:13:58 +0900

    fix: 프롬포트 수정 및 녹음파일 표시 형식 수정

--- frontend/src/api/script.tsx
+++ frontend/src/api/script.tsx
@@ -8,7 +8,7 @@ type AsyncFunction = (
 export const makeAnalysisScript: AsyncFunction = async(genderSelected,ageSelected) => {
   try{
     const response: AxiosResponse<string> = await privateApi.get<string>(
-      `/practice/diction/script?cmd=성별이${ageSelected}이고 ${genderSelected}인 더빙 대사를 실제 드라마나 영화 애니메이션 등에 사용되었던 스크립트로 20자 이상 60자 이내로 작성해줘`
+      `/practice/diction/script?cmd=성별이${ageSelected}이고 ${genderSelected}인 스크립트를 15초 내외 분량으로 무조건 특수기호 없이 그리고 무조건 스크립트라는 설명없이 작성해줘`
     )
     console.log(genderSelected)
     console.log(ageSelected)
@@ -24,7 +24,7 @@ export const makeAccentScript: AsyncFunction = async(categorySelected) => {
   try{
     console.log(categorySelected+"    api")
     const response: AxiosResponse<string> = await privateApi.get<string>(
-      `/practice/diction/script?cmd=${categorySelected} 의 해당하는 대사를 실제 영화나 드라마에 다왔던 대사 한 줄글로 내놔 어떤 스크립트인지는 알려주지 말고 ':' 표시 없고 특수문자 없이 40자 이상 60자 이내로`
+      `/practice/diction/script?cmd=${categorySelected} 와 관련된 스크립트를 꼭 15초 이내 분량으로 무조건 특수기호 없이 그리고 무조건 스크립트라는 설명없이 작성해줘. 실제 대본처럼 작성해줘`
     )
     return response.data
   }
